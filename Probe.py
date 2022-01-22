5efce62fa62eba5729358662d1a4950988b21263
Igor Moroz <igor.moroz@gmail.com>
2022-01-23 01:37:55 +0300

    Last changes

--- Probe.py
+++ Probe.py
@@ -1,12 +1,21 @@
 while True:
-    grats_template = input('Введите шаблон поздравления {name} и {age}: ')
-    if '{name}' in grats_template and {age} in grats_template:
+    grats_template = input('Введите шаблон поздравления, в шаблоне нужно использовать конструкцию {name} и {age}: ')
+    if '{name}' in grats_template and '{age}' in grats_template:
         break
-    else:
-        print('Ошибка. Отсутствует одна или две конструкции')
+    print('Ошибка: отсутствует одна или две конструкции.')
 
 names_list = input('Список людей через запятую: ').split(', ')
-ages_str =  input('Возраст людей через пробел: ')
-ages = [int(i_number) for i_number in ages_str.split()]
-for i_name in names_list:
-    print(grats_template.format(name=i_name))
+ages_srt = input('Возраст людей через пробел: ')
+ages = [int(i_number) for i_number in ages_srt.split()]
+
+for i_man in range(len(names_list)):
+    print(grats_template.format(name=names_list[i_man], age=ages[i_man]))
+
+people = [
+    ' '.join([names_list[i_man], str(ages[i_man])])
+    for i_man in range(len(names_list))
+]
+
+people_str = ', '.join(people)
+
+print('\nИменинники:', people_str)
