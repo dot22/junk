0679d3dcab351854822a96c71926280d2e8ab7ee
Igor Moroz <igor.moroz@gmail.com>
2022-01-24 21:06:06 +0300

    Last changes

--- Probe.py
+++ Probe.py
@@ -1,10 +1,8 @@
-def crypt(symbol):
-    return abc[(abc.index(symbol) + shift) % 33]
+details_num = 50000000
+price = 23.8589578
+increase = 0.045678
 
-abc = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-text = input('Введите шифрумое предложение: ')
-shift = int(input('Введите сдвиг: '))
-
-crypt_text = ''.join([crypt(i_sym) if i_sym != ' ' else ' ' for i_sym in text])
-
-print(crypt_text)
+print('На складе {:,d} деталей'.format(details_num))
+print('Каждая деталь стоит {:.2f} рублей'.format(price))
+print('Цена увеличилась на {:.1%}'.format(increase))
+print('На складе {:.0e} деталей'.format(details_num))
