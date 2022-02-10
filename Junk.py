4830b4f2a4c3963579e9ea08ee721ca50fe5fa29
Igor Moroz <igor.moroz@gmail.com>
2022-02-10 16:50:42 +0300

    data exchange between servers

--- Junk.py
+++
@@ -1,9 +0,0 @@
-import random
-
-numbers_list = [random.randint(0,4) for _ in range(10)]
-new_list = []
-for i_num in numbers_list:
-    if i_num not in new_list:
-        new_list.append(i_num)
-print(numbers_list)
-print(new_list)
--- Probe.py
+++ Probe.py
@@ -1,37 +1,12 @@
-text = input('Введите текст: ')
-original_dict = {}
+orders = int(input('Введите количество заказов: '))
+cust_dict = {}
 
-for i_sym in text:
-    if i_sym in original_dict:
-        original_dict[i_sym] += 1
-    else:
-        original_dict[i_sym] = 1
-
-# original_dict = {
-#     ' ': 2,
-#     '-': 1,
-#     'З': 1,
-#     'а': 2,
-#     'д': 1,
-#     'е': 1,
-#     'и': 1,
-#     'н': 2,
-#     'о': 3,
-#     'п': 1,
-#     'с': 2,
-#     'т': 2,
-#     'ч': 1,
-#     'ь': 1
-# }
-
-invert_dict = dict.fromkeys(original_dict.values())
-
-for i_key in invert_dict:
-    invert_dict[i_key] = []
-    for i_sym in original_dict:
-        if original_dict[i_sym] == i_key:
-            invert_dict[i_key].append(i_sym)
-
-print('Инвертированный словарь частот:')
-for i_num in sorted(invert_dict.keys()):
-    print(i_num, ':', invert_dict[i_num])
+for i_order in range(1, orders + 1):
+    print(i_order, 'заказ: ', end='')
+    order = input('').split(' ')
+    print(cust_dict.keys())
+    if order[0] in cust_dict.keys() and order[1] in cust_dict[order[0]].keys():
+        # print(cust_dict[order[0]].get(order[1]))
+        order[2] += int(cust_dict[order[0]].get(order[1]))
+    cust_dict[order[0]] = {order[1]: int(order[2])}
+    print(cust_dict)
