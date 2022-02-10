85aa094886350fb1c8a701c262447e92e0547c59
Igor Moroz <igor.moroz@gmail.com>
2022-02-10 22:46:30 +0300

    exchange

--- Probe.py
+++ Probe.py
@@ -4,9 +4,6 @@ cust_dict = {}
 for i_order in range(1, orders + 1):
     print(i_order, 'заказ: ', end='')
     order = input('').split(' ')
-    print(cust_dict.keys())
-    if order[0] in cust_dict.keys() and order[1] in cust_dict[order[0]].keys():
-        # print(cust_dict[order[0]].get(order[1]))
-        order[2] += int(cust_dict[order[0]].get(order[1]))
-    cust_dict[order[0]] = {order[1]: int(order[2])}
+    order[2] = int(order[2])
+    cust_dict[order[0]][order[1]] = order[2]
     print(cust_dict)
