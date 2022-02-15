ac0e04bad3755beb0c4a233638baec4c273a5f4f
Igor Moroz <igor.moroz@gmail.com>
2022-02-15 03:02:57 +0300

    exchange

--- Probe.py
+++ Probe.py
@@ -1,21 +1,18 @@
-number = int(input('Введите максимальное число: '))
-number_set = {str(i) for i in range(1, number + 1)}
+number = int(input('Введите количество человек: '))
+family_dict = dict()
+family_range = dict()
+count = 0
+
+for i_num in range(1, number):
+    print(i_num, 'пара: ', end='')
+    pair = input('').split(' ')
+    print(pair)
+    family_dict[pair[0]] = pair[1]
+
+family_keys = set(family_dict.keys())
+family_values = set(family_dict.values())
+parent = family_values.difference(family_keys)
+print('parent', parent)
 
 while True:
-    if len(number_set) == 1:
-        print('Нужное число найдено:', *number_set)
-        break
-
-    print('\nНужное число есть среди вот этих чисел: ', end='')
-    search = input()
-    if search == 'Помогите!':
-        print('Артем мог загадать следующие числа:', ' '.join(sorted(number_set)))
-        break
-    else:
-        search_set = set(search.split(' '))
-
-    answer = input('Ответ Артема: ')
-    if answer == 'Да':
-        number_set = number_set.intersection(search_set)
-    elif answer == 'Нет':
-        number_set = number_set.difference(search_set)
+    family_range[parent] = count
