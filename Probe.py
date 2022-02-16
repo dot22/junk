49829a537e0e61c88df97248f8f5504137d24af1
Igor Moroz <igor.moroz@gmail.com>
2022-02-16 15:58:04 +0300

    data exchange between servers

--- Probe.py
+++ Probe.py
@@ -1,12 +1,15 @@
-number = int(input('Введите количество человек: '))
-family_dict = dict()
+# number = int(input('Введите количество человек: '))
+number = 9
+# family_dict = dict()
+family_dict = {'Alexei': 'Peter_I', 'Anna': 'Peter_I', 'Elizabeth': 'Peter_I', 'Peter_II': 'Alexei', 'Peter_III': 'Anna', 'Paul_I': 'Peter_III', 'Alexander_I': 'Paul_I', 'Nicholaus_I': 'Paul_I'}
+
 family_range = dict()
 count = 1
 
-for i_num in range(1, number):
-    print(i_num, 'пара: ', end='')
-    pair = input('').split(' ')
-    family_dict[pair[0]] = pair[1]
+# for i_num in range(1, number):
+#     print(i_num, 'пара: ', end='')
+#     pair = input('').split(' ')
+#     family_dict[pair[0]] = pair[1]
 
 family_keys = set(family_dict.keys())
 family_values = set(family_dict.values())
@@ -14,12 +17,25 @@ parent = family_values.difference(family_keys)
 root = parent.pop()
 family_range[root] = 0
 
+
+# print(family_dict)
+# print(family_range)
+# print(family_keys)
+# print(family_values)
+
 while len(family_range) != number:
+    print('len family range - ', len(family_range))
+    print('family_dict', family_dict)
     for i_fam in family_dict:
+        # print(i_fam)
         if family_dict.get(i_fam) in family_range.keys() and i_fam not in family_range:
+            # print(i_fam, family_dict.get(i_fam), family_range.keys(), family_range)
+            # print(count)
             family_range[i_fam] = count
     count += 1
 
-print('\n"Высота" каждого члена семьи:')
-for i_name in sorted(family_range):
-    print(i_name, family_range[i_name])
+print('family_range -', family_range)
+#
+# print('\n"Высота" каждого члена семьи:')
+# for i_name in sorted(family_range):
+#     print(i_name, family_range[i_name])
