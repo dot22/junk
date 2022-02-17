f4f93f10b6d253439b97b0949dc0bb46d2767134
Igor Moroz <igor.moroz@gmail.com>
2022-02-17 16:33:55 +0300

    data exchange between servers

--- Probe.py
+++ Probe.py
@@ -1,10 +1,10 @@
 # number = int(input('Введите количество человек: '))
 number = 9
-# family_dict = dict()
+family_dict = dict()
 family_dict = {'Alexei': 'Peter_I', 'Anna': 'Peter_I', 'Elizabeth': 'Peter_I', 'Peter_II': 'Alexei', 'Peter_III': 'Anna', 'Paul_I': 'Peter_III', 'Alexander_I': 'Paul_I', 'Nicholaus_I': 'Paul_I'}
-
+# family_dict = {'8': '7', '7': '6', '6': '5', '5': '4', '4': '3', '3': '2', '2': '1'}
 family_range = dict()
-count = 1
+count = 0
 
 # for i_num in range(1, number):
 #     print(i_num, 'пара: ', end='')
@@ -15,7 +15,7 @@ family_keys = set(family_dict.keys())
 family_values = set(family_dict.values())
 parent = family_values.difference(family_keys)
 root = parent.pop()
-family_range[root] = 0
+family_range[root] = count
 
 
 # print(family_dict)
@@ -23,19 +23,19 @@ family_range[root] = 0
 # print(family_keys)
 # print(family_values)
 
-while len(family_range) != number:
-    print('len family range - ', len(family_range))
-    print('family_dict', family_dict)
+for _ in range(number):
+# while len(family_range) != number:
+    # print('len family range - ', len(family_range))
+    # print('family_dict', family_dict)
     for i_fam in family_dict:
-        # print(i_fam)
-        if family_dict.get(i_fam) in family_range.keys() and i_fam not in family_range:
-            # print(i_fam, family_dict.get(i_fam), family_range.keys(), family_range)
-            # print(count)
-            family_range[i_fam] = count
+        if (family_dict.get(i_fam) in family_range) and not (i_fam in family_range):
+            print(i_fam, family_dict.get(i_fam))
+            family_range[i_fam] = count + 1
+            print(family_range[i_fam])
     count += 1
 
 print('family_range -', family_range)
-#
-# print('\n"Высота" каждого члена семьи:')
-# for i_name in sorted(family_range):
-#     print(i_name, family_range[i_name])
+
+print('\n"Высота" каждого члена семьи:')
+for i_name in sorted(family_range):
+    print(i_name, family_range[i_name])
