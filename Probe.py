ef7604778010ff37feffaed129c6cacf78e5b79b
Igor Moroz <igor.moroz@gmail.com>
2022-02-22 07:00:57 +0300

    exchange

--- Probe.py
+++ Probe.py
@@ -1,8 +1,8 @@
-from math import pi
+def user_script(string):
+    user_list = [i_sym for i_num, i_sym in enumerate(string) if i_num % 2 == 0]
+    return user_list
 
-def side(r, h):
-    surf_side = 2 * pi * r * h
-    full_size =
-radius = int(input('Введите радиус: '))
-heigh = int(input('Введите высоту: '))
-
+string1 = 'О Дивный Новый мир!'
+string2 = [100, 200, 300, 'буква', 0, 2, 'a']
+print(user_script(string1))
+print(user_script(string2))
