78ff60e3f742beee03f7529fac451a7c5a662179
Igor Moroz <igor.moroz@gmail.com>
2022-03-02 06:49:57 +0300

    exchange

--- Probe.py
+++ Probe.py
@@ -1,18 +1,19 @@
-import random
+phone_book = {}
 
-
-original_list = [random.randint(0, 99) for x in range(10)]
-print('Оригинальный список:', original_list)
-
-# print('\nВариант 1')
-# new_list_1 = []
-# for i_index in range(0, 10, 2):
-#     list_for_add = (original_list[i_index], original_list[i_index + 1])
-#     new_list_1.append(list_for_add)
-# print(new_list_1)
-
-print('\nВариант 2')
-new_list_2 = []
-for i_count, i_data in enumerate(original_list):
-    print(i_count, i_data)
-
+while True:
+    action = input('\nВведите действие:\n"Добавить контакт" (нажмите 0)\n\tили\n"Поиск человека по фамилии" (нажмите 1): ')
+    if action == '0':
+        person = tuple(input('Имя, Фамилия: ').split(' '))
+        if person in phone_book.keys():
+            print('Такой контакт уже есть в телефонной книге')
+        else:
+            phone_number = input('Номер телефона: ')
+            phone_book[person] = phone_number
+        print(phone_book)
+    elif action == '1':
+        search_name = input('Введите фамилию для поиска')
+        for i_name in phone_book:
+            if search_name in i_name:
+                print(i_name, phone_book[i_name])
+    else:
+        break
