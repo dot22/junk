122183f2228e3643313773857396e4fa790d91ab
Igor Moroz <igor.moroz@gmail.com>
2022-03-02 21:01:06 +0300

    exchange

--- Probe.py
+++ Probe.py
@@ -22,4 +22,14 @@ score_dict = {
 #     score_dict[i_count] = record
 
 print(score_dict)
-
+score_rank = {int(i_value[0]): i_key  for i_key, i_value in score_dict.items()}
+top_scores = sorted(score_rank)
+top_score_1 = top_scores[-1]
+top_score_2 = top_scores[-2]
+top_score_3 = top_scores[-3]
+# print(top_scores)
+print(score_rank)
+print(sorted(score_rank))
+print(top_score_1)
+print(top_score_2)
+print(top_score_3)
