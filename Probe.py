3222b0e12c345643718bf8778a4e733af455aefb
Igor Moroz <igor.moroz@gmail.com>
2022-03-03 16:45:14 +0300

    data exchange between servers

--- Probe.py
+++ Probe.py
@@ -1,11 +1,4 @@
-some_string = 'abcd'
-some_tuple = (10, 20, 30, 40)
+def factorial(num):
 
-print('Строка:', some_string)
-print('Кортеж чисел:', some_tuple)
-
-print('\nРезультат:')
-result = ((some_string[i], some_tuple[i]) for i in range(len(some_string)))
-print(result)
-for i_index in result:
-    print(i_index)
+num_fact = factorial(5)
+print(num_fact)
