4b2dde7840b596974f26c9d3b3c132889ef8f5eb
Igor Moroz <igor.moroz@gmail.com>
2022-03-03 16:23:25 +0300

    data exchange between servers

--- Probe.py
+++ Probe.py
@@ -1,45 +1,11 @@
-## test data
-# score_dict = {
-#     1: ('69485', 'Jack'),
-#     2: ('95715', 'qwerty'),
-#     3: ('95715', 'Alex'),
-#     4: ('83647', 'M'),
-#     5: ('197128', 'qwerty'),
-#     6: ('95715', 'Jack'),
-#     7: ('93289', 'Alex'),
-#     8: ('95715', 'Alex'),
-#     9: ('95715', 'M')
-# }
+some_string = 'abcd'
+some_tuple = (10, 20, 30, 40)
 
-count = int(input('Сколько записей вносится в протокол? '))
-score_dict = {}
+print('Строка:', some_string)
+print('Кортеж чисел:', some_tuple)
 
-print('Записи (результат и имя):')
-
-## составляем словарь из записей состоявшихся игр
-for i_count in range(1, count + 1):
-    print(i_count, 'запись: ', end='')
-    record = tuple(input().split(' '))
-    score_dict[i_count] = record
-
-## сортируем очки состоявшихся игр в обратном порядке
-score_rates = sorted([int(i_value[0]) for i_key, i_value in score_dict.items()], reverse=True)
-
-top_list = {}
-top_users = []
-
-## перебирая поочередно список очков состоявшихся игр, распределяем игры по убыванию счета в игре
-## удаляя дублирующиеся имена игроков
-for i_rate, i_score in enumerate(score_rates):
-    for i_value in score_dict.values():
-        user_score = i_value[0]
-        user_name = i_value[1]
-        if i_score == int(user_score) and user_name not in top_users:
-            top_list[i_rate + 1] = (user_name, user_score)
-            top_users.append(user_name)
-            break
-
-## выводим на печать первые три призовых места
-print('\nИтоги соревнований:')
-for i_top in range(1, 4):
-    print(i_top, ' место. ', top_list.get(i_top)[0], ' (', top_list.get(i_top)[1], ')', sep='')
+print('\nРезультат:')
+result = ((some_string[i], some_tuple[i]) for i in range(len(some_string)))
+print(result)
+for i_index in result:
+    print(i_index)
