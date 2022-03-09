feef00d36049389dd78979ca9d6b1e011e7f5acc
Igor Moroz <igor.moroz@gmail.com>
2022-03-09 16:31:09 +0300

    data exchange between servers

--- Probe.py
+++ Probe.py
@@ -1,7 +1,19 @@
-def factorial(num):
-    if num == 1:
-        return 1
-    return num * factorial(num - 1)
+list_1 = [1, 2, 3, 4]
+list_2 = 'word'
 
-num_fact = factorial(5)
-print(num_fact)
+for i in range(4):
+    print([value_1 for index_1, value_1 in enumerate(list_1)][i])
+    print([value_2 for index_2, value_2 in enumerate(list_2)][i])
+
+for index_1, value_1 in enumerate(list_1):
+    print(index_1, value_1)
+
+for index_2, value_2 in enumerate(list_2):
+    print(index_2, value_2)
+
+# list_f1 = [value_1 for index_1, value_1 in enumerate(list_1)]
+# list_f2 = [value_2 for index_2, value_2 in enumerate(list_2)]
+# print(list_f1)
+# print(list_f2)
+# list_f = [(value_1 for index_1, value_1 in enumerate(list_1)), (value_2 for index_2, value_2 in enumerate(list_2)) for i in range(4) if i == index_2]
+# print(list_f)
