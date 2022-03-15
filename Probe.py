3d03149dea1db376f220ca85abea8f8101e84319
Igor Moroz <igor.moroz@gmail.com>
2022-03-15 18:03:20 +0300

    data exchange between servers

--- Probe.py
+++ Probe.py
@@ -1,14 +1,20 @@
-def search_key(dbase, key, deep=0):
-    if key in dbase:
-        return dbase[key]
+def search_key(dbase, key, deep):
+    if deep != 0:
+        if key in dbase:
+            return dbase[key]
 
-    for sub_base in dbase.values():
-        if isinstance(sub_base, dict):
-            result = search_key(sub_base, key)
-            if result:
-                break
+        for sub_base in dbase.values():
+            if isinstance(sub_base, dict):
+                result = search_key(sub_base, key, deep)
+                deep -= 1
+                print(deep)
+                if result:
+                    break
+        else:
+            result = None
     else:
-        result = None
+        print('Достигнута максимальная глубина')
+        return
 
     return result
 
@@ -27,8 +33,11 @@ site = {
 }
 
 user_key = input('Какой ключ ищем? ')
-user_deep = int(input('На какую глубину искать (по-умолчанию - без ограничения)? '))
+
+user_deep = int(input('На какую глубину искать (по-умолчанию - без ограничения)? ') or '-1')
+
 value = search_key(site, user_key, user_deep)
+
 if value:
     print(value)
 else:
