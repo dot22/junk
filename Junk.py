1acf4a68fd09bc8483ff8bf7d4030a91469eca3b
Igor Moroz <igor.moroz@gmail.com>
2022-03-29 18:52:28 +0300

    data exchange between servers

---
+++ Junk.py
@@ -0,0 +1,9 @@
+def summ(*data):
+    result = 0
+    for i in data:
+       result += i
+    return result
+
+
+# print('summ(1, 2, 3, 4, 5)')
+print('Ответ:', summ(1, 2, 3, 4, 5))
--- Probe.py
+++ Probe.py
@@ -1,17 +1,17 @@
-def sum(data, result=0):
-    for i_count, i_value in enumerate(data):
+def summ(data):
+    result = 0
+    for i_value in data:
         if isinstance(i_value, int):
-            result += i_value
+            i_summ = i_value
         else:
-            sub_data = i_value
-            sum(sub_data, result)
+            i_summ = summ(i_value)
+        result += i_summ
     return result
 
 
-list_1 = [[1, 2, [3]], [4], 5]
+list_1 = [[1, 2, [3]], [1], 3]
 list_2 = 1, 2, 3, 4, 5
-
-sum(list_1)
-sum(list_2)
-print(sum(list_1))
-# print(sum(list_2))
+print(list_1)
+print('Ответ:', summ(list_1))
+print(list_2)
+print('Ответ:', summ(list_2))
