d542db696eb36732071bb2287bcc35950598aed9
Igor Moroz <igor.moroz@gmail.com>
2022-04-07 16:59:57 +0300

    data exchange between servers

--- Probe.py
+++ Probe.py
@@ -1,6 +1,15 @@
-def move(disk, stick_i, stick)
+import os
 
-number = 2
-list_stick = [1, 2, 3]
-sti = [x for x in range(1, number + 1)]
-print(list_number)
+def find_file(path,file):
+    if os.path.exists(os.path.join(path, file)):
+        print(os.path.join(path, file))
+
+    if os.path.isdir(os.path.join(path, file)):
+        sub_dir = os.path.join(path, file)
+        find_file(sub_dir, file)
+
+# abs_path = 'd:\yandex\git'
+abs_path = 'd:\yandex\git\junk.w'
+file_name = 'Junk.py'
+
+find_file(abs_path, file_name)
