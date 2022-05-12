744809a2c2bff024cee661ca9670568773a83b55
Igor Moroz <igor.moroz@gmail.com>
2022-05-12 20:57:54 +0300

    data exchange between servers

--- Probe.py
+++ Probe.py
@@ -1,7 +1,25 @@
-speakers_file = open('speakers.txt', 'r', encoding='utf-8')
-data = speakers_file.read(14)
-speakers_file.seek(1)
-data_2 = speakers_file.read(22)
-print(data)
-print(data_2)
-speakers_file.close()
+import os
+
+
+def read_file(path, file):
+    for i_elem in os.listdir(path):
+        if i_elem == file:
+
+
+    if os.path.exists(os.path.join(path, file)):
+        print(os.path.join(path, file))
+        file_read = open(os.path.join(path, file), 'r')
+        data = file_read.read()
+        print(data)
+        return file_read.close()
+        # return print(data)
+    for i_list in os.listdir(path):
+        if os.path.isdir(os.path.join(path, i_list)):
+            sub_path = os.path.join(path, i_list)
+            read_file(sub_path, file)
+
+
+path_search = '/home/dot/Yandex/Obsidian'
+file_search = 'Template.md'
+
+read_file(path_search, file_search)
