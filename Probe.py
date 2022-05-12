7384931084bbbf94b942a08a4b70b0f3ac5b3cda
Igor Moroz <igor.moroz@gmail.com>
2022-05-12 21:02:48 +0300

    data exchange between servers

--- Probe.py
+++
@@ -1,7 +0,0 @@
-speakers_file = open('speakers.txt', 'r', encoding='utf-8')
-data = speakers_file.read(14)
-speakers_file.seek(1)
-data_2 = speakers_file.read(22)
-print(data)
-print(data_2)
-speakers_file.close()
