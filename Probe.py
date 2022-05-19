1b006925ee33558ef59aa97f530aad4d0b92d722
Igor Moroz <igor.moroz@gmail.com>
2022-05-19 20:23:07 +0300

    from home

--- Probe.py
+++ Probe.py
@@ -1,1 +1,11 @@
-Probe
+numbers_file = open('numbers.txt', 'r')
+anser_file = open('answer.txt', 'w')
+summ = 0
+
+for i_number in numbers_file.read().split():
+    summ += int(i_number)
+
+anser_file.write(str(summ))
+#
+numbers_file.close()
+anser_file.close()
