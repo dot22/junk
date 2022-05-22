0410d6826187699852bc963fb98883ce6be21213
Igor Moroz <igor.moroz@gmail.com>
2022-05-22 23:43:06 +0300

    from home

--- Probe.py
+++ Probe.py
@@ -1,11 +1,15 @@
-numbers_file = open('numbers.txt', 'r')
-anser_file = open('answer.txt', 'w')
-summ = 0
+import os
 
-for i_number in numbers_file.read().split():
-    summ += int(i_number)
 
-anser_file.write(str(summ))
-#
-numbers_file.close()
-anser_file.close()
+try:
+    ages = open('ages.txt', 'r')
+    result = open('result.txt', 'x')
+    for i_line in ages:
+        age_round = round(i_line, 2)
+        result.write(age_round)
+except FileExistsError:
+    print('Файл с таким именем уже существует')
+except IsADirectoryError:
+    print('Под указанным имненем существует каталог, но не файл')
+except TypeError:
+    print('Некорректный тип данных или некорретное значение')
