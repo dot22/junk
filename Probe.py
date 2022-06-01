871a0698fc5bb6b3feac74aea691f73dfe76c6d7
Igor Moroz <igor.moroz@gmail.com>
2022-06-01 05:19:36 +0300

    update .gitignore

--- Probe.py
+++ Probe.py
@@ -1,40 +1,16 @@
-def divide(number):
-    return 10 / number
+file = open('words.txt', 'r')
+palindrom = 0
 
-
-def sum_of_divided(left, right):
-    div_sum = 0
-    for i_num in range(left, right + 1):
-        try:
-            div_sum += divide(i_num)
-            print(div_sum)
-        except ZeroDivisionError:
-            print('На ноль делить нельзя')
-    return div_sum
-
-
-total = 2
 try:
-    numbers_file = open('numbers.txt', 'r')
-    for i_line in numbers_file:
-        nums_list = i_line.split()
-        first_num = int(nums_list[0])
-        second_num = int(nums_list[1])
-
-        total += sum_of_divided(first_num, second_num)
-    print('Общая сумма:', total)
-
-except ZeroDivisionError:
-    print('На ноль делить нельзя')
-
-answer_file = open('answer.txt', 'w')
-try:
-    answer_file.write(int('The answer is: '))
-    answer_file.write(total)
+    for i_word in file:
+        if i_word.endswith('\n'):
+            i_word = i_word[:-1]
+        if not i_word.isalpha():
+            raise TypeError
+        else:
+            if i_word == i_word[::-1]:
+                palindrom += 1
 except TypeError:
-    print('Ошибка записи в файл: тип данных не строка')
-else:
-    print('Программа выполнилась без ошибок')
-finally:
-    answer_file.close()
-    print(answer_file.closed)
+    print('В слове {} содержится цифра'.format(i_word))
+
+print('Число палиндромов:', palindrom)
