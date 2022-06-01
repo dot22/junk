a5d5d80d7167dfd99fe93f201ca18ecea97164e5
Igor Moroz <igor.moroz@gmail.com>
2022-06-01 18:13:10 +0300

    from work

---
+++ Probe.py
@@ -0,0 +1,38 @@
+import random
+
+
+def x_divide_y(x, y):
+    try:
+        x += random.randint(0, 10)
+        y += random.randint(0, 5)
+        return x / y
+    except ZeroDivisionError:
+        print('Ошибка в первой функции: на ноль делить нельзя')
+
+
+def y_divide_x(x, y):
+    try:
+        x -= random.randint(0, 10)
+        y -= random.randint(0, 5)
+        return y / x
+    except ZeroDivisionError:
+        print('Ошибка во второй функции: на ноль делить нельзя')
+
+
+try:
+    with open('coordinates.txt', 'r') as file, open('result.txt', 'w') as file_2:
+    # file = open('coordinates.txt', 'r')
+    # file_2 = open('result.txt', 'w')
+        for line in file:
+            nums_list = line.split()
+            res1 = x_divide_y(int(nums_list[0]), int(nums_list[1]))
+            res2 = y_divide_x(int(nums_list[0]), int(nums_list[1]))
+            number = random.randint(0, 100)
+            my_list = sorted([res1, res2, number])
+            print(' '.join(map(str, my_list)))
+            file_2.write(' '.join(map(str, my_list)) + '\n')
+except:
+    print("Что-то пошло не так с первой функцией")
+# finally:
+#     file.close()
+#     file_2.close()
