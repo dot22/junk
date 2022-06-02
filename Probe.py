7998b3c88e37516744113df2f9d0b60779c8ed5c
Igor Moroz <igor.moroz@gmail.com>
2022-06-02 22:44:46 +0300

    from home

--- Probe.py
+++ Probe.py
@@ -1,58 +1,32 @@
-import random
-
-
-sym_sum = 0
-line_count = 0
-
-try:
-    with open('people.txt', 'r') as peoples_file:
-        for i_line in peoples_file:
-            line_count += 1
-            length = len(i_line)
-            if i_line.endswith('\n'):
-                length -= 1
-            if length < 3:
-                raise BaseException('Длина {} строки меньше трех символов'.format(line_count))
-            sym_sum += length
-
-except BaseException:
-    print('Test')
-finally:
-    print(sym_sum)
-
-
-def x_divide_y(x, y):
+def validation(data, number):
+    personal_data = data.split()
     try:
-        x += random.randint(0, 10)
-        y += random.randint(0, 5)
-        return x / y
-    except ZeroDivisionError:
-        print('Ошибка в первой функции: на ноль делить нельзя')
+        if len(personal_data) != 3:
+            raise IndexError
+        if not personal_data[0].isalpha():
+            raise NameError
+        if ('@' or '.') not in personal_data[1]:
+            raise SyntaxError
+        # if not personal_data[2].isdigit() or not (10 <= int(personal_data[2]) <= 99):
+        if not (10 <= int(personal_data[2]) <= 99):
+            raise ValueError
+    except IndexError:
+        bad_log.write('Строка ' + str(number) + ':' + str(personal_data) + ' - НЕ присутствуют все три поля\n')
+    except NameError:
+        bad_log.write('Строка ' + str(number) + ':' + str(personal_data) + ' - поле имени содержит НЕ только буквы\n')
+    except SyntaxError:
+        bad_log.write('Строка ' + str(number) + ':' + str(personal_data) + ' - поле емейл НЕ содержит @ и . (точку)\n')
+    except ValueError:
+        bad_log.write('Строка ' + str(number) + ':' + str(personal_data) + ' - поле возраст НЕ является числом от 10 до 99\n')
+    else:
+        good_log.write(' '.join(personal_data) + '\n')
 
 
-def y_divide_x(x, y):
-    try:
-        x -= random.randint(0, 10)
-        y -= random.randint(0, 5)
-        return y / x
-    except ZeroDivisionError:
-        print('Ошибка во второй функции: на ноль делить нельзя')
+line_number = 0
 
-
-try:
-    with open('coordinates.txt', 'r') as file, open('result.txt', 'w') as file_2:
-    # file = open('coordinates.txt', 'r')
-    # file_2 = open('result.txt', 'w')
-        for line in file:
-            nums_list = line.split()
-            res1 = x_divide_y(int(nums_list[0]), int(nums_list[1]))
-            res2 = y_divide_x(int(nums_list[0]), int(nums_list[1]))
-            number = random.randint(0, 100)
-            my_list = sorted([res1, res2, number])
-            print(' '.join(map(str, my_list)))
-            file_2.write(' '.join(map(str, my_list)) + '\n')
-except:
-    print("Что-то пошло не так с первой функцией")
-# finally:
-#     file.close()
-#     file_2.close()
+with open('registrations.txt', 'r') as registration, \
+        open('registrations_bad.log', 'w') as bad_log, \
+        open('registrations_good.log', 'w') as good_log:
+    for user_data in registration:
+        line_number += 1
+        validation(user_data, line_number)
