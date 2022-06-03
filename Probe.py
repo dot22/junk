499e5db7615b8a3ebb03fcc16be3612c85f3d1fe
Igor Moroz <igor.moroz@gmail.com>
2022-06-03 06:44:37 +0300

    from home

--- Probe.py
+++ Probe.py
@@ -1,32 +1,33 @@
-def validation(data, number):
-    personal_data = data.split()
+def func(line):
     try:
-        if len(personal_data) != 3:
-            raise IndexError
-        if not personal_data[0].isalpha():
-            raise NameError
-        if ('@' or '.') not in personal_data[1]:
-            raise SyntaxError
-        # if not personal_data[2].isdigit() or not (10 <= int(personal_data[2]) <= 99):
-        if not (10 <= int(personal_data[2]) <= 99):
-            raise ValueError
-    except IndexError:
-        bad_log.write('Строка ' + str(number) + ':' + str(personal_data) + ' - НЕ присутствуют все три поля\n')
-    except NameError:
-        bad_log.write('Строка ' + str(number) + ':' + str(personal_data) + ' - поле имени содержит НЕ только буквы\n')
-    except SyntaxError:
-        bad_log.write('Строка ' + str(number) + ':' + str(personal_data) + ' - поле емейл НЕ содержит @ и . (точку)\n')
-    except ValueError:
-        bad_log.write('Строка ' + str(number) + ':' + str(personal_data) + ' - поле возраст НЕ является числом от 10 до 99\n')
-    else:
-        good_log.write(' '.join(personal_data) + '\n')
+        members = line.split(' ')
+        first_operand = members[0]
+        if not first_operand.isdigit():
+            raise TypeError
+        else:
+            first_operand = int(first_operand)
+        second_operand = members[2]
+        if not second_operand.isdigit():
+            raise TypeError
+        else:
+            second_operand = int(second_operand)
+        op = members[1]
+        op_list = '+-*/^'
+        if len(op) > 1:
+            raise ArithmeticError
+        elif op not in op_list:
+            raise ArithmeticError
+        else:
+            cmd = r'print({} {} {})'.format(first_operand, op, second_operand)
 
 
-line_number = 0
+summ = 0
 
-with open('registrations.txt', 'r') as registration, \
-        open('registrations_bad.log', 'w') as bad_log, \
-        open('registrations_good.log', 'w') as good_log:
-    for user_data in registration:
-        line_number += 1
-        validation(user_data, line_number)
+with open('calc.txt', 'r') as calc:
+    for i_string in calc:
+        if i_string.endswith('\n'):
+            i_string = i_string[:-1]
+        result_of_string = func(i_string)
+        # summ += result_of_string
+
+print('Сумма результатов:', summ)
