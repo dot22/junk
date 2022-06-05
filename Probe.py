36996989f5cb03064cb1dc00171d1bfdcf21e333
Igor Moroz <igor.moroz@gmail.com>
2022-06-05 14:08:38 +0300

    from home

--- Probe.py
+++ Probe.py
@@ -1,39 +1,47 @@
 def func(line):
     try:
+        if line.endswith('\n'):
+            line = line[:-1]
+
         members = line.split(' ')
         first_operand = members[0]
+        operation = members[1]
+        second_operand = members[2]
+
         if not first_operand.isdigit():
             raise TypeError
         else:
             first_operand = int(first_operand)
-        second_operand = members[2]
+
         if not second_operand.isdigit():
             raise TypeError
         else:
             second_operand = int(second_operand)
-        op = members[1]
-        op_list = '+-*/^'
-        if len(op) > 1:
+
+        operation_list = '+-*/^'
+        if len(operation) > 1:
             raise ArithmeticError
-        elif op not in op_list:
-            raise ArithmeticError
+        elif operation not in operation_list:
+            raise TypeError
         else:
-            cmd = 'print({} {} {})'.format(first_operand, op, second_operand)
-            print(cmd)
-            eval(cmd)
-            # print(exec(cmd))
-    except (TypeError, ArichmeticError):
-        print('fin')
-    return eval(cmd)
+            cmd = '{} {} {}'.format(first_operand, operation, second_operand)
+            return eval(cmd)
+
+    except (ArithmeticError, TypeError):
+        print('Обнаружена ошибка в строке:', line)
+        answer = input('Хотите исправить? ').lower()
+        if answer == 'нет':
+            return 0
+        else:
+            fixed_line = input('Введите исправленную строку: ')
+            return func(fixed_line)
+
 
 summ = 0
 
 with open('calc.txt', 'r') as calc:
     for i_string in calc:
-        if i_string.endswith('\n'):
-            i_string = i_string[:-1]
         result_of_string = func(i_string)
-        print(result_of_string)
         summ += result_of_string
 
 print('Сумма результатов:', summ)
