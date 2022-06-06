9be21dc0ec245289657bb96d49ab865c19c65fc6
Igor Moroz <igor.moroz@gmail.com>
2022-06-06 15:46:27 +0300

    from work

--- Probe.py
+++ Probe.py
@@ -1,10 +1,29 @@
+def read():
+    chat_read = open('chat.txt', 'r', encoding='utf-8')
+    for i_line in chat_read:
+        print(i_line, end='')
+    chat_read.close()
+    return
+
+
+def write():
+    chat_write = open('chat.txt', 'a', encoding='utf-8')
+    text = input('Отправить сообщение: ')
+    text_to_chat = (name + ': ' + text + '\n')
+    chat_write.write(text_to_chat)
+    chat_write.close()
+    return
+
+
 name = input('Введите имя: ')
 
+print('Привет,', name)
+
 while True:
-    answer = input('Выберите действие: 1 - просмотреть текущий текст чата, 2 - отправить сообщение: ')
+    answer = input('\nВыберите действие: 1 - просмотреть текущий текст чата, 2 - отправить сообщение: ')
     if answer == '1':
-        chat = open('chat.txt', 'r')
-        for i_line in chat:
-            print(i_line)
+        read()
+    elif answer == '2':
+        write()
     else:
-        print('выход')
+        print('Ошибка в выборе действия')
