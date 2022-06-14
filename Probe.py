d6e5c74efb674e5ae350e063bd05262a2c578118
Igor Moroz <igor.moroz@gmail.com>
2022-06-14 06:17:08 +0300

    from home

--- Probe.py
+++ Probe.py
@@ -1,16 +1,20 @@
 class Employee:
+    name = 'Tom'
+    salary = 10000
 
-    def __init__(self, name, salary):
+    def __int__(self, name, salary):
         self.name = name
         self.salary = salary
 
     def print_info(self):
         print(
-            'Name: {}\nSalary: {}'.format(self.name, self.salary)
+            'Name: {} \nSalary: {}'.format(self.name, self.salary)
         )
 
 
-emp_1 = Employee('Tom', 10000)
-emp_2 = Employee('Bob', 20000 )
+emp_1 = Employee()
 emp_1.print_info()
+emp_2 = Employee()
+emp_2.name = 'Bob'
+emp_2.salary = 20000
 emp_2.print_info()
