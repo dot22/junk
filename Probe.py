1e8d78a24b34ac4c99d2b68e23ed8d6060809a8c
Igor Moroz <igor.moroz@gmail.com>
2022-07-27 21:39:47 +0300

    from home

--- Probe.py
+++ Probe.py
@@ -1,8 +1,36 @@
-from garden import PotatoGarden
+class Potato:
+    states = {0: 'Отсутствует', 1: 'Росток', 2: 'Зеленая', 3: 'Зрелая'}
+
+    def __init__(self, index):
+        self.index = index
+        self.state = 0
+
+    def info(self):
+        print('Статус картошки {} - {}'.format(self.index, self.state))
+
+    def ruse(self):
+        if self.state < 3:
+            self.state += 1
+            print('Картошка {} сейчас {}'.format(self.index, Potato.states[self.state]))
+        else:
+            print('Вся картошка созрела. Можно собирать!')
 
 
-my_garden = PotatoGarden(3)
-my_garden.is_all_ripe()
-for _ in range(3):
-    my_garden.grow_all()
-    my_garden.is_all_ripe()
+class PotatoGarden:
+
+    def __init__(self, count):
+        self.potatoes = [Potato(index) for index in range(1, count + 1)]
+
+    def info(self):
+
+
+
+
+
+# potato_1 = Potato(1)
+# potato_1.info()
+# for _ in range(4):
+#     potato_1.ruse()
+
+my_garden = PotatoGarden(5)
+print(PotatoGarden)
