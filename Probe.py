28497f646f29488c7b4fd6bcca19e88a923c736c
dot22 <igor.moroz@gmail.com>
2022-11-23 20:15:02 +0300

    from work with graphic interface

--- Probe.py
+++ Probe.py
@@ -1,5 +1,5 @@
-a = input('Введите первое слово: ')
-b = input('Введите второе слово: ')
-print(a, b)
-a, b = b, a
-print(a, b)
+a = '2'
+b = '5'
+c = '3'
+num = 6 ** int(a) + (7 - int(b)) * int(c)
+print(num)
