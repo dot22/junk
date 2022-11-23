60ff735fc5579cf299b8f3a31571f30f946b53c9
dot22 <igor.moroz@gmail.com>
2022-11-23 17:30:29 +0300

    from work with graphic interface

--- Probe.py
+++ Probe.py
@@ -1,2 +1,9 @@
-first = 'Моя первая переменная!'
-print(first)
+name = 'Иван'
+my_color = 'Зелёный'
+sport = 'Бег'
+print('Меня зовут')
+print(name)
+print('Мой любимый цвет:')
+print(my_color)
+print('Мой любимый вид спорта:')
+print(sport)
