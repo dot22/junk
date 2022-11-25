023e451062ad25dfd9b9c29e6290a2823f19c5ec
dot22 <igor.moroz@gmail.com>
2022-11-25 16:25:20 +0300

    from work with graphic interface

--- Draft.py
+++
@@ -1,6 +0,0 @@
-time = int(input('Enter the time: '))
-
-if (time < 8) or (time >= 22) or (time == 14) or (10 <= time < 12) or (18 <= time < 20):
-    print('Bad time')
-else:
-    print('Good time')
--- Probe.py
+++
@@ -1,6 +0,0 @@
-time = int(input('Enter the time: '))
-
-if (8 <= time < 22) and ((time != 14) or (10 <= time < 12) or (18 <= time < 20)):
-    print('Good time')
-else:
-    print('Bad time')
