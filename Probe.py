11d502e4d16868b97c69c4fb13125194cd6c0389
dot22 <igor.moroz@gmail.com>
2022-11-25 12:12:33 +0300

    from work with graphic interface

--- Probe.py
+++ Probe.py
@@ -1,5 +1,14 @@
-temperature = int(input('Введите температуру: '))
-if temperature < 0 or temperature > 100:
-    print('Alarm')
-else:
-    print('Good condition')
+level1 = 1000
+level2 = 2500
+level3 = 5000
+
+count = int(input('Введите количество опыта: '))
+
+if count > level3:
+    print('Ваш уровень: 3')
+elif count > level2:
+    print('Ваш уровень:', 2)
+elif count > level1:
+    print('Ваш уровень: 1')
+elif count < level1:
+    print('Ваш уровень: 0')
