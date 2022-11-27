aff2f2de96b7fa4786bbbc28cd712475a07898a7
Igor Moroz <igor.moroz@gmail.com>
2022-11-28 00:02:04 +0300

    7.7 homework - task 01

--- Probe.py
+++ Probe.py
@@ -1,9 +1,6 @@
-bags = int(input('How many bags: '))
-total_weight = 0
-count_bags = 0
-while count_bags < bags:
-    count_bags += 1
-    weight = int(input('Bags weight: '))
-    total_weight += weight
-    print('Перенесено', count_bags, 'из', bags)
-print('Общий вес:', total_weight)
+number = int(input('Number: '))
+count = 1
+while count <= number:
+    print(count, 'в кубе = ', count ** 3)
+    count += 1
+
