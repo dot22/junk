dcb11788fc10532c0a06d2f9a7550d0366994e26
dot22 <igor.moroz@gmail.com>
2022-11-28 10:01:57 +0300

    7 homework - task 05

--- Probe.py
+++ Probe.py
@@ -1,8 +1,10 @@
-count = 0
-while True:
-    sequence = int(input('Enter next seq: '))
-    if sequence == 0:
-        break
-    elif (sequence % 2) == 0:
-        count += 1
-print(count)
+number = int(input('Enter the number: '))
+
+number1 = number // 1000
+number2 = number % 1000
+sum1 = number1 % 10 + number1 // 10 % 10 + number1 // 100
+sum2 = number2 % 10 + number2 // 10 % 10 + number2 // 100
+if sum1 == sum2:
+    print('Good Luck!')
+else:
+    print('You loose')
