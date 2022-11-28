4d80c022921a46c301e9004361e8c75bb21d6b56
dot22 <igor.moroz@gmail.com>
2022-11-28 16:14:17 +0300

    7 homework - task 08

--- Probe.py
+++ Probe.py
@@ -1,18 +1,14 @@
-work_time = 8
-count = 1
-tasks = 0
-wife_call = False
-print('Начался 8-часовой рабочий день')
+# bank_account = int(input('Bank account: '))
+bank_account = 10
+# percent = int(input('Percent: '))
+percent = 10
+# max_amount = int(input('Max amount: '))
+max_amount = 50
+years = 0
 
-while count <= work_time:
-    print(count, '-й час', sep='')
-    count += 1
-    jobs_assigned = int(input('How many jobs assigned: '))
-    tasks += jobs_assigned
-    wife_calling = int(input('Wife is calling. Answer the phone? 0/1 '))
-    if wife_calling == 1:
-        wife_call = True
+while bank_account < max_amount:
+    years += 1
+    bank_account = (bank_account + bank_account * percent / 100) * 10000 // 100 / 100
+    print(bank_account)
 
-print('Work time ended. Tasks done -', tasks)
-if wife_call:
-    print('Make shopping')
+print(years)
