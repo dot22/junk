7d04c66b507c3e9f3bedd9ddb643b101800ea896
Igor Moroz <igor.moroz@gmail.com>
2022-11-30 00:16:08 +0300

    8.3.1

--- Probe.py
+++ Probe.py
@@ -1,6 +1,2 @@
-winners = 0
-for number in 345, 19, 87, 1020, 421:
-    if (99 < number < 1000) and number % 5 == 0:
-        print('win number -', number)
-        winners += 1
-print('Winners -', winners)
+for i in range(11):
+    print(i ** 2)
