c6fc4ce91b97a9f21f732f8f90d06c26a7e26197
dot22 <igor.moroz@gmail.com>
2022-11-29 16:29:37 +0300

    7 homework - task 10

--- Probe.py
+++ Probe.py
@@ -1,13 +1,20 @@
-secret_number = 7
+number = 50
+number_min = 0
+number_max = 100
 count = 0
 
 while True:
     count += 1
-    number = int(input('Enter the number: '))
-    if number > secret_number:
-        print('Число больше, чем нужно. Попробуйте ещё раз!')
-    elif number < secret_number:
-        print('Число меньше, чем нужно. Попробуйте ещё раз!')
-    else:
-        print('Вы угадали! Число попыток:', count)
+    print('Попытка номер', count)
+    print('Твоё число равно, меньше или больше, чем число ', number, '?', sep='')
+    answer = int(input('1 — равно, 2 — больше, 3 — меньше: '))
+    if answer == 1:
+        print('Your number is', number)
+        print('Game is over!')
         break
+    elif answer == 2:
+        number_min = number
+        number = (number_max + number_min) // 2
+    else:
+        number_max = number
+        number = (number_max + number_min) // 2
