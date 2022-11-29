f01981c32e320af6b7bd63d0835bbcd9e90fae50
Igor Moroz <igor.moroz@gmail.com>
2022-11-30 00:04:23 +0300

    8.2.1

--- Probe.py
+++ Probe.py
@@ -1,20 +1,5 @@
-number = 50
-number_min = 0
-number_max = 100
-count = 0
-
-while True:
-    count += 1
-    print('Попытка номер', count)
-    print('Твоё число равно, меньше или больше, чем число ', number, '?', sep='')
-    answer = int(input('1 — равно, 2 — больше, 3 — меньше: '))
-    if answer == 1:
-        print('Your number is', number)
-        print('Game is over!')
-        break
-    elif answer == 2:
-        number_min = number
-        number = (number_max + number_min) // 2
+for meters in 100,90,95,87,102:
+    if meters % 3 == 0:
+        print(meters, 'Подходит')
     else:
-        number_max = number
-        number = (number_max + number_min) // 2
+        print(meters, 'Не подходит')
