6191f5b874ef3bc10734035894472a6ce7580de7
Igor Moroz <igor.moroz@gmail.com>
2022-11-30 00:33:50 +0300

    8.4.3

--- Probe.py
+++ Probe.py
@@ -1,6 +1,16 @@
-num1 = int(input('Number 1: '))
-num2 = int(input('Number 2: '))
-summ = 0
-for i in range(num1, num2 + 1):
-    summ += i
-print(summ)
+wake_up = int(input('Wake up hour: '))
+hour_without_dream = 0
+call_summ = 0
+
+for i in range(wake_up, 23):
+    print('Now is', i, 'hour')
+    call = int(input('How many callories: '))
+    call_summ += call
+    if call_summ >= 2000:
+        print('Enough')
+        break
+    hour_without_dream += 1
+
+print('Callories: ', call_summ)
+print('Hour without dream: ', hour_without_dream)
+
