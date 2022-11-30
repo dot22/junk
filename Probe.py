252e2286e3165b5207d7760fccd86590cb4da1d8
dot22 <igor.moroz@gmail.com>
2022-11-30 09:11:36 +0300

    8.6.7

--- Probe.py
+++ Probe.py
@@ -1,19 +1,11 @@
-numbers = 3, 5, 4, 3, 3, 4, 5, 3, 3, 4, 4, 3, 4
-n3 = 0
-n4 = 0
-n5 = 0
+a = int(input('A: '))
+b = int(input('B: '))
+summ = 0
+count = 0
 
-for i in numbers:
-    if i == 3:
-        n3 += 1
-    elif i == 4:
-        n4 += 1
-    else:
-        n5 += 1
+for i in range(a, b + 1):
+    if i % 3 == 0:
+        count += 1
+        summ += i
 
-if n3 > n4 and n3 > n5:
-    print('n3')
-elif n4 > n3 and n4 > n5:
-    print('n4')
-else:
-    print('n5')
+print('Answer:', summ / count)
