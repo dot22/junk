f8b4cda3c7cf71158dde7a5f8f6c2fff35b3d104
dot22 <igor.moroz@gmail.com>
2022-11-30 09:23:12 +0300

    8.6.9

--- Probe.py
+++ Probe.py
@@ -1,6 +1,14 @@
-for i in range(10, 100):
-    n10 = i // 10
-    n1 = i % 10
-    s = 3 * (n1 * n10)
-    if i == s:
-        print(i)
+min_sal = 0
+flag = True
+
+for i in range(10):
+    cur_sal = int(input('Current salary: '))
+    if cur_sal < min_sal:
+        flag = False
+    else:
+        min_sal = cur_sal
+
+if flag:
+    print('Good salary')
+else:
+    print('Bad salary')
