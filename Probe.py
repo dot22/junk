54b1daa6ad42e6b6b82cd1b9591f2d4e0a5136ef
dot22 <igor.moroz@gmail.com>
2022-11-30 09:07:24 +0300

    8.6.6

--- Probe.py
+++ Probe.py
@@ -1,8 +1,19 @@
-number = int(input('Enter the number: '))
-factorial = 1
+numbers = 3, 5, 4, 3, 3, 4, 5, 3, 3, 4, 4, 3, 4
+n3 = 0
+n4 = 0
+n5 = 0
 
-for i in range(1, number + 1):
-    factorial *= i
-    print(i, factorial)
+for i in numbers:
+    if i == 3:
+        n3 += 1
+    elif i == 4:
+        n4 += 1
+    else:
+        n5 += 1
 
-print('Факториал от', number, 'равен', factorial)
+if n3 > n4 and n3 > n5:
+    print('n3')
+elif n4 > n3 and n4 > n5:
+    print('n4')
+else:
+    print('n5')
