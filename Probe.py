a4f2b21084428c27968fff537931d80400e02bd2
dot22 <igor.moroz@gmail.com>
2022-11-30 08:08:30 +0300

    8.6.4

--- Probe.py
+++ Probe.py
@@ -1,6 +1,11 @@
-sallary = 0
+count = 0
 
-for i in range(12):
-    month = int(input('Enter the sallary for the month: '))
-    sallary += month
-print('Average month salary =', sallary / 12)
+for i in range(30, 36):
+    print('Людей в секторе', i, end='')
+    puple = int(input(': '))
+    if puple <= 10:
+        print('Всё спокойно')
+    else:
+        print('Нарушение!')
+        count += 1
+print('Нарушений -', count)
