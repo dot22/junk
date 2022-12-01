f3bb2a9321bfedce7e9fed9188da4721f99248e3
dot22 <igor.moroz@gmail.com>
2022-12-01 17:17:45 +0300

    9.6.7

--- Probe.py
+++ Probe.py
@@ -1,9 +1,14 @@
-a = int(input('Enter the size: '))
-count = 0
-envelop = 12 * 12
+scholarship = int(input('Enter the scholarship: '))
+expenses = int(input('Enter the expenses: '))
+full_expenses = 0
 
-while a ** 2 > envelop:
-    count += 1
-    a = a / 2
+for month in range(10):
+    if month != 0:
+        expenses = expenses + expenses * 3 / 100
+    full_expenses += expenses
 
-print(count)
+full_scholarship = scholarship * 10
+
+summ = full_expenses - full_scholarship
+print(summ)
+
