33e00e2b2f99a7d3014f7f4522f1ffc2ed42947b
dot22 <igor.moroz@gmail.com>
2022-12-01 09:31:42 +0300

    9.4.3

--- Probe.py
+++ Probe.py
@@ -1,9 +1,4 @@
-soldiers = int(input('How many soldiers: '))
-rules = int(input('How many rules: '))
-summ = 0
-for soldier in range(soldiers - 4, 0, -4):
-    ask = int(input('How many rules you know: '))
-    if ask != rules:
-        print('You must make', soldier * 10, 'pushups!')
-        summ += soldier * 10
-print('Total pushups:', summ)
+sec = int(input('How many secs: '))
+
+for i in range(sec - sec % 2, 0, -2):
+    print(i)
