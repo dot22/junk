bce314185310ef204d8c2a598b12c296d49f5512
dot22 <igor.moroz@gmail.com>
2022-12-02 10:12:47 +0300

    9.6.10

--- Probe.py
+++ Probe.py
@@ -1,8 +1,9 @@
-x = int(input('X: '))
-res = 1
-for i in range(1, 65):
-    if i % 2 != 0:
-        res *= (x - i)
-    else:
-        res /= (x - i)
-print(res)
+girls = int(input('How many girls? '))
+boys = int(input('How many boys? '))
+
+if (girls > 2 * boys) or (boys > 2 * girls):
+    print('Решения нет')
+elif girls >= boys:
+    print((girls - boys) * 'GBG' + (boys - (girls - boys)) * 'BG')
+else:
+    print((boys - girls) * 'BGB' + (girls - (boys - girls)) * 'GB')
