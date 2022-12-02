117f7f6a5c2ab33c42e0137daffecfedb3ff02db
dot22 <igor.moroz@gmail.com>
2022-12-02 10:37:09 +0300

    10.2.3

--- Probe.py
+++ Probe.py
@@ -1,5 +1,6 @@
+name = input('What is your name? ')
+
+print(name, 'купи слона')
 while True:
-    question = input('Have job done? ')
-    if question == 'Yes, of course':
-        break
-print('Good job')
+    answer = input('')
+    print('Все говорят', answer, ',а ты купи слона')
