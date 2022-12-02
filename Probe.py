949106a594f9c43f8d8546a0f47dd9a59cf020ff
dot22 <igor.moroz@gmail.com>
2022-12-02 11:07:08 +0300

    10.3.2

--- Probe.py
+++ Probe.py
@@ -1,4 +1,12 @@
 text = input('Enter the text: ')
+count_big = 0
+count_small = 0
 
 for i in text:
-    print(3 * i)
+    if i == 'Ы':
+        count_big += 1
+    elif i == 'ы':
+        count_small += 1
+
+print('Big:', count_big)
+print('Small:', count_small)
