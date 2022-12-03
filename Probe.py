641ea4dbb335bdcd2338704b004c239c59ffe706
Igor Moroz <igor.moroz@gmail.com>
2022-12-03 23:42:14 +0300

    10.6.6

--- Probe.py
+++ Probe.py
@@ -1,25 +1,14 @@
-x_start = 1
-x_end = 15
-x = 8
-y_start = 1
-y_end = 20
-y = 10
+string = input('Enter the string: ')
+summ = 0
+summ_max = 0
 
-while True:
-    print('Марсоход находится на позиции ', x, ', ', y, ', введите команду:', sep='')
-    command = input('Команда: ')
-    if command == 'a' or command == 'A':
-        if x - 1 >= x_start:
-            x -= 1
-    if command == 'd' or command == 'D':
-        if x + 1 <= x_end:
-            x += 1
-    if command == 'w' or command == 'W':
-        if y + 1 <= y_end:
-            y += 1
-    if command == 's' or command == 'S':
-        if y - 1 >= y_start:
-            y -= 1
-    if command == '0':
-        break
+for letter in string:
+    if summ_max < summ:
+        summ_max = summ
+    if letter == 's':
+        summ += 1
+    else:
+        summ = 0
+
+print(summ_max)
 
