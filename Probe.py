88179e8f3eb093e8e4d268ef51bdc71a99394e97
Igor Moroz <igor.moroz@gmail.com>
2022-12-03 23:35:33 +0300

    10.6.5

--- Probe.py
+++ Probe.py
@@ -1,6 +1,25 @@
-rows = int(input('Enter number of rows: '))
-seats = int(input('Enter number of seats: '))
-space = int(input('Free space in meters: '))
+x_start = 1
+x_end = 15
+x = 8
+y_start = 1
+y_end = 20
+y = 10
 
-for i in range(rows):
-    print('=' * seats + '*' * space + '=' * seats)
+while True:
+    print('Марсоход находится на позиции ', x, ', ', y, ', введите команду:', sep='')
+    command = input('Команда: ')
+    if command == 'a' or command == 'A':
+        if x - 1 >= x_start:
+            x -= 1
+    if command == 'd' or command == 'D':
+        if x + 1 <= x_end:
+            x += 1
+    if command == 'w' or command == 'W':
+        if y + 1 <= y_end:
+            y += 1
+    if command == 's' or command == 'S':
+        if y - 1 >= y_start:
+            y -= 1
+    if command == '0':
+        break
+
