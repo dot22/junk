85d88b06c1c4b0cc1484f4875986360b08d0fe39
Igor Moroz <igor.moroz@gmail.com>
2022-12-04 22:07:46 +0300

    11.6.3

--- Probe.py
+++ Probe.py
@@ -1,7 +1,12 @@
-number = int(input('Enter the number: '))
+x = int(input('Enter X size: '))
+y = int(input('Enter Y size: '))
 
-for row in range(1, number + 1):
-    for col in range(1, number + 1):
-        if row >= col:
-            print(row, end='')
+for row in range(x):
+    for col in  range(y):
+        if col == 0 or col == x -1:
+            print('|', end='')
+        elif row == 0 or row == y - 1:
+            print('-', end='')
+        else:
+            print(' ', end='')
     print()
