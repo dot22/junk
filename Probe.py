35034c0efd5bac83ee39cece83d0ad7918380bc5
Igor Moroz <igor.moroz@gmail.com>
2022-12-04 22:00:34 +0300

    11.6.1

--- Probe.py
+++ Probe.py
@@ -1,7 +1,4 @@
-number = int(input('Enter the number: '))
-
-for row in range(number + 1):
-    for col in range(row, number + 1):
-        if row <= col:
-            print(col, end=' ')
+for row in range(6):
+    for col in range(6):
+        print(row + col * 2, end='\t')
     print()
