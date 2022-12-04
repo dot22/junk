3c1fe3ebd0a281af3a9cd9401d7368ec026e57f4
Igor Moroz <igor.moroz@gmail.com>
2022-12-04 17:43:59 +0300

    11.3.2

--- Probe.py
+++ Probe.py
@@ -1,9 +1,13 @@
 for row in range(20):
-    for col in range(30):
-        if row == 0:
-            print('-', end='')
-        elif col == 0 or col == 29:
+    for col in range(50):
+        if col == 24:
             print('|', end='')
+        elif col == row + 29:
+            print('\\', end='')
+        elif col == -row + 19:
+            print('/', end='')
+        elif row == 9:
+            print('-', end='')
         else:
             print(' ', end='')
     print()
