87afd532dd10f143e01c6b0508ec3658ede067c7
Igor Moroz <igor.moroz@gmail.com>
2022-12-04 11:51:57 +0300

    11.2.3

--- Probe.py
+++ Probe.py
@@ -1,9 +1,9 @@
-size = int(input('Enter the size: '))
-
-for row in range(1, size + 1):
-    for col in range(1, size + 1):
-        if col % 3 == 0:
-            print(col, end=' ')
+for row in range(20):
+    for col in range(50):
+        if col == 24:
+            print('|', end='')
+        elif row == 9:
+            print('-', end='')
         else:
-            print(row, end=' ')
+            print(' ', end='')
     print()
