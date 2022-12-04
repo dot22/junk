8c85f490d49584a2305339ca69cd9a738e176cc7
Igor Moroz <igor.moroz@gmail.com>
2022-12-04 11:31:02 +0300

    11.1.1

--- Probe.py
+++ Probe.py
@@ -1,13 +1,4 @@
-string = 'shacnidw'
-word1 = ''
-word2 = ''
-position = 0
-
-for letter in string:
-    position += 1
-    if position % 2 == 1:
-        word1 = word1 + letter
-    else:
-        word2 = letter + word2
-
-print(word1 + word2)
+for row in range(1, 10):
+    for col in range(1, 10):
+        print(row * col, end='\t')
+    print()
