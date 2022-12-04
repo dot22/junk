4be6b04bf25c51c42f549805e85d929f8883273a
Igor Moroz <igor.moroz@gmail.com>
2022-12-04 11:45:55 +0300

    11.2.1

--- Probe.py
+++ Probe.py
@@ -1,6 +1,10 @@
-number = 10
+size = 5
 
-for row in range(number):
-    for col in range(number):
-        print(row - col, end='\t')
+for row in range(1, size + 1):
+    for col in range(1, size + 1):
+        if row % 2 == 0:
+            print(row, end=' ')
+        else:
+            print(col, end=' ')
     print()
+
