8449f24dbb4327d8bc10da858016cc59bb35fc8d
dot22 <igor.moroz@gmail.com>
2022-12-05 14:59:41 +0300

    11.6.3 - update

--- Probe.py
+++ Probe.py
@@ -1,9 +1,9 @@
 x = int(input('Enter X size: '))
 y = int(input('Enter Y size: '))
 
-for row in range(x):
-    for col in  range(y):
-        if col == 0 or col == x -1:
+for row in range(y):
+    for col in range(x):
+        if col == 0 or col == x - 1:
             print('|', end='')
         elif row == 0 or row == y - 1:
             print('-', end='')
