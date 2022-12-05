816345f4abfd9214af9efe9588173712c1b80287
dot22 <igor.moroz@gmail.com>
2022-12-05 15:30:46 +0300

    11.6.5

--- Probe.py
+++ Probe.py
@@ -1,11 +1,14 @@
-size = 11
+numbers = int(input('Enter the number: '))
+count = 0
 
-for row in range(size):
-    for col in range(size + 1):
-        if row == col:
-            print('^', end='')
-        elif row == size - col:
-            print('^', end='')
-        else:
-            print(' ', end='')
-    print()
+for number in range(numbers):
+    simple = False
+    numeral = int(input('Enter numeral: '))
+    for divisor in range(2, numeral):
+        if numeral % divisor == 0:
+            simple = True
+    if simple == False:
+        count += 1
+
+
+print(count)
