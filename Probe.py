1174529206c3285cf01bde55a49d6107ba330e1b
dot22 <igor.moroz@gmail.com>
2022-12-05 16:30:33 +0300

    11.6.7

--- Probe.py
+++ Probe.py
@@ -1,9 +1,16 @@
-number = int(input('Enter the number: '))
-summ = 0
+amount_numbers = int(input('Enter numbers: '))
+max_number = 0
+max_summ = 0
 
-for i in range(1, number + 1):
-    for j in range(1, i):
-        i *= j
-    summ += i
+for order in range(amount_numbers):
+    number = int(input('Enter the number: '))
+    number_summ = 0
+    test_number = number
+    while test_number > 0:
+        number_summ += test_number % 10
+        test_number //= 10
+    if number_summ > max_summ:
+        max_summ = number_summ
+        max_number = number
 
-print(summ)
+print(max_number, max_summ)
