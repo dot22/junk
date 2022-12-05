0e277f8e813c0b47a5960ba7b821f755a7ceab59
dot22 <igor.moroz@gmail.com>
2022-12-05 16:35:11 +0300

    11.6.8

--- Probe.py
+++ Probe.py
@@ -1,16 +1,4 @@
-amount_numbers = int(input('Enter numbers: '))
-max_number = 0
-max_summ = 0
+size = int(input('Enter the size of the piramid: '))
 
-for order in range(amount_numbers):
-    number = int(input('Enter the number: '))
-    number_summ = 0
-    test_number = number
-    while test_number > 0:
-        number_summ += test_number % 10
-        test_number //= 10
-    if number_summ > max_summ:
-        max_summ = number_summ
-        max_number = number
-
-print(max_number, max_summ)
+for i in range(size):
+    print((size - i - 1) * ' ' + (i * 2 + 1) * '*')
