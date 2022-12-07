fde459917c6be3a7d73be8b8a8b39e4edc213474
Igor Moroz <igor.moroz@gmail.com>
2022-12-07 22:38:44 +0300

    12.3.3

--- Probe.py
+++ Probe.py
@@ -1,4 +1,5 @@
-x = float(input('X: '))
+# x = float(input('X: '))
+x = 0.731
 
 while True:
     if x < 0 or x > 1:
@@ -7,8 +8,8 @@ while True:
     else:
         break
 
-
-y = float(input('Y: '))
+# y = float(input('Y: '))
+y = 0.167
 
 while True:
     if y < 0 or y > 1:
@@ -18,3 +19,20 @@ while True:
         break
 
 print('Your coordinates:', int(x * 10), int(y * 10))
+# print(int(x * 10))
+# print(x * 10)
+#
+# print(int(y * 10))
+# print(y * 10)
+
+if int(x * 10) + 0.5 > x * 10:
+    x_center = round(((int(x * 10) + 0.5 - x * 10) / 10), 3)
+else:
+    x_center = (x * 10 - int(x * 10) + 0.5)
+
+if int(y * 10) + 0.5 > y * 10:
+    y_center = round(((int(y * 10) + 0.5 - y * 10) / 10), 3)
+else:
+    y_center = -round((y * 10 - int(y * 10) - 0.5) / 10, 3)
+print(x_center)
+print(y_center)
