4d84a10066be8118eb44aeed612cc523e38d81d6
Igor Moroz <igor.moroz@gmail.com>
2022-12-07 22:52:16 +0300

    12.4.1

--- Probe.py
+++ Probe.py
@@ -1,38 +1,12 @@
-# x = float(input('X: '))
-x = 0.731
+import math
 
-while True:
-    if x < 0 or x > 1:
-        print('X wrong. Try another time:')
-        x = float(input('X: '))
-    else:
-        break
 
-# y = float(input('Y: '))
-y = 0.167
+a = float(input('a: '))
+b = float(input('b: '))
+c = float(input('c: '))
 
-while True:
-    if y < 0 or y > 1:
-        print('Y wrong. Try another time')
-        y = float(input('Y: '))
-    else:
-        break
+p = (a + b + c) / 2
 
-print('Your coordinates:', int(x * 10), int(y * 10))
-# print(int(x * 10))
-# print(x * 10)
-#
-# print(int(y * 10))
-# print(y * 10)
+s = math.sqrt(p * (p - a) * (p - b) * (p - c))
 
-if int(x * 10) + 0.5 > x * 10:
-    x_center = round(((int(x * 10) + 0.5 - x * 10) / 10), 3)
-else:
-    x_center = (x * 10 - int(x * 10) + 0.5)
-
-if int(y * 10) + 0.5 > y * 10:
-    y_center = round(((int(y * 10) + 0.5 - y * 10) / 10), 3)
-else:
-    y_center = -round((y * 10 - int(y * 10) - 0.5) / 10, 3)
-print(x_center)
-print(y_center)
+print('S =', s)
