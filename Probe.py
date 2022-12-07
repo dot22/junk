0de7875e750b50fe3c864bc95d79156470bbe22c
dot22 <igor.moroz@gmail.com>
2022-12-07 16:20:16 +0300

    12.2.3

--- Probe.py
+++ Probe.py
@@ -1,4 +1,13 @@
-age = int(input('Age: '))
-temperature = float(input('Temperature: '))
+height = float(input('Heigh: '))
+weight = float(input('Weigh: '))
 
-print('Present: ', age * temperature)
+bmi = round(weight / height ** 2, 2)
+print(bmi)
+if bmi < 18.5:
+    print('Not enough')
+elif bmi < 25:
+    print('Good')
+elif bmi < 30:
+    print('Much')
+else:
+    print('Too much')
