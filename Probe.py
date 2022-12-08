7513f0e05e224c6a2bc34aa9c829b979404a444a
dot22 <igor.moroz@gmail.com>
2022-12-08 16:45:49 +0300

    12.6.5

--- Probe.py
+++ Probe.py
@@ -1,9 +1,13 @@
 import math
 
+radius = float(input('Radius of the planet: '))
+earth_volume = 10.8321e11
 
-# number = float(input('Number: '))
-number = 123.98
+planet_volume = 4 / 3 * math.pi * radius ** 3
 
-digit = math.floor(number * 10) % 10
-print(number)
-print(digit)
+if planet_volume < earth_volume:
+    print('Объём планеты Земля больше в', round(earth_volume / planet_volume, 3), 'раз')
+else:
+    inverse_coefficient = round(earth_volume / planet_volume, 3)
+    coefficient = round(planet_volume / earth_volume, 3)
+    print('Объём планеты Земля меньше в (1/', inverse_coefficient, ') = ', coefficient, 'раз', sep='')
