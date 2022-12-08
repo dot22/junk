c8947ed64ae66e2fea142e51d0bd0af9d7d658c9
dot22 <igor.moroz@gmail.com>
2022-12-08 16:33:37 +0300

    12.6.4

--- Probe.py
+++ Probe.py
@@ -1,17 +1,9 @@
 import math
 
 
-size = int(input('Enter file size: '))
-speed = int(input('Enter connection speed: '))
-count = 0
-approximate_size = 0
+# number = float(input('Number: '))
+number = 123.98
 
-while approximate_size < size:
-    count += 1
-    if approximate_size + speed < size:
-        approximate_size += speed
-    else:
-        approximate_size = size
-    percent_size = math.ceil(approximate_size / size * 100)
-    print('Прошло ', count, ' сек. Скачано ', approximate_size, ' из ', size, ' Мб (', percent_size, '%)', sep='')
-
+digit = math.floor(number * 10) % 10
+print(number)
+print(digit)
