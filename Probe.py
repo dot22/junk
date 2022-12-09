76d47259570af966439f27258a8605fb58951154
Igor Moroz <igor.moroz@gmail.com>
2022-12-10 00:11:21 +0300

    12.6.7

--- Probe.py
+++ Probe.py
@@ -1,17 +1,25 @@
-# bottom_c = int(input('Нижняя граница: '))
-bottom_c = 0
-# top_c = int(input('Верхняя граница: '))
-top_c = 70
-# step = int(input('Шаг: '))
-step = 20
+# x_horse = float(input('Horse X: '))
+# y_horse = float(input('Horse Y: '))
+x_horse = 0.071
+y_horse = 0.118
 
-print('\nВывод')
-print('C\tF')
+# x_new = float(input('New X: '))
+# y_new = float(input('New Y :'))
+x_new = 0.213
+y_new = 0.068
 
-for t_c in range(bottom_c, top_c, step):
-    t_f = int(32 + t_c * 1.8)
-    print(t_c, t_f, sep='\t')
-    if t_c + step > top_c:
-        t_f = int(32 + top_c * 1.8)
-        print(top_c, t_f, sep='\t')
+x_hp = int(x_horse * 10)
+y_hp = int(y_horse * 10)
+
+x_nn = int(x_new * 10)
+y_nn = int(y_new * 10)
+
+if (abs(x_hp - x_nn) + abs(y_hp - y_nn) == 3) and (x_hp != x_nn and y_hp != y_nn):
+    print('Good choise')
+else:
+    print('Wrong point')
+
+print('Previous point: ', x_hp, y_hp)
+print('Next point: ', x_nn, y_nn)
+
 
