144e53e2c416446a292500e047925ae452e878c8
Igor Moroz <igor.moroz@gmail.com>
2022-12-09 23:52:49 +0300

    12.6.6

--- Probe.py
+++ Probe.py
@@ -1,7 +1,7 @@
 # bottom_c = int(input('Нижняя граница: '))
 bottom_c = 0
 # top_c = int(input('Верхняя граница: '))
-top_c = 50
+top_c = 70
 # step = int(input('Шаг: '))
 step = 20
 
@@ -11,3 +11,7 @@ print('C\tF')
 for t_c in range(bottom_c, top_c, step):
     t_f = int(32 + t_c * 1.8)
     print(t_c, t_f, sep='\t')
+    if t_c + step > top_c:
+        t_f = int(32 + top_c * 1.8)
+        print(top_c, t_f, sep='\t')
+
