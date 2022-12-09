9d5069cac4d2f23f153f678c04dafffb7bc36e36
Igor Moroz <igor.moroz@gmail.com>
2022-12-10 01:52:36 +0300

    12.6.10

--- Probe.py
+++ Probe.py
@@ -1,22 +1,11 @@
-import math
+a = int(input('A: '))
+b = int(input('B: '))
 
-a = float(input('A: '))
-b = float(input('B: '))
-c = float(input('C: '))
+c = a + b
+d = a - b
 
-d = (b ** 2) - (4 * a * c)
+min = (c - abs(d)) / 2
+max = min + abs(d)
 
-print('D =', d)
+print(min, max)
 
-if d < 0:
-    print('No way')
-elif d == 0:
-    x = (-1 * b) / (2 * a)
-    print('X =', x)
-else:
-    x1 = (-1 * b + math.sqrt(d)) / (2 * a)
-    x2 = (-1 * b - math.sqrt(d)) / (2 * a)
-    if x1 < x2:
-        print(x1, x2)
-    else:
-        print(x2, x1)
