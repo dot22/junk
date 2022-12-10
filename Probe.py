84c7860cbeac69df73352811c1c8e1e04173251e
Igor Moroz <igor.moroz@gmail.com>
2022-12-10 22:46:02 +0300

    13.3.2

--- Probe.py
+++ Probe.py
@@ -1,8 +1,14 @@
-def price(number):
-    print('Цена:', number)
+import math
 
-for i in range(3):
-    print('Название: КлирВотер')
-    print('Производитель: ВодЗавод')
-    cost = int(input('Введите цену: '))
-    price(cost)
+
+def sphere(radius):
+    print('Площадь сферы:', 4 * math.pi * radius ** 2 )
+
+
+def yellow(radius):
+    print('Объем шара:', 4 / 3 * math.pi * radius ** 3)
+
+
+r = float(input('Введите радиус планеты: '))
+sphere(r)
+yellow(r)
