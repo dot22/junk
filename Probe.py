4638c311069b46c3da1f1da3c51f438e793e1176
Igor Moroz <igor.moroz@gmail.com>
2022-12-11 22:09:12 +0300

    13.3.3

--- Probe.py
+++ Probe.py
@@ -1,14 +1,16 @@
-import math
+def isprime(amount):
+    summ = 0
+    for order in range(amount):
+        a = int(input("Введите число: "))
+        flag = 0
+        for i in range(2, a // 2 + 1):
+            if a % i == 0:
+                flag = 1
+                break
+        if flag == 0:
+            summ += 1
+    print('Summ =', summ)
 
 
-def sphere(radius):
-    print('Площадь сферы:', 4 * math.pi * radius ** 2 )
-
-
-def yellow(radius):
-    print('Объем шара:', 4 / 3 * math.pi * radius ** 3)
-
-
-r = float(input('Введите радиус планеты: '))
-sphere(r)
-yellow(r)
+numbers = int(input('Введите количество чисел: '))
+isprime(numbers)
