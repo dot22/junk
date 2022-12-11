d2d654ef3c89854cbd5c887f6b38c1b56130ea39
Igor Moroz <igor.moroz@gmail.com>
2022-12-11 22:51:00 +0300

    13.4.3

--- Probe.py
+++ Probe.py
@@ -1,10 +1,24 @@
-def address(surname, name, country, city, street, house, flat):
-    print('Фамилия: ', surname)
-    print('Имя: ', name)
-    print('Страна: ', country)
-    print('Город', city)
-    print('Улица: ', street)
-    print('Номер дома: ', house)
-    print('Номер квартиры: ', flat)
+import math
 
-address('Ibanov', 'Ivan', 'Russia', 'Piter', 'Kozlova', '12', 52)
+
+def distance(x, y):
+    print('Дистанция:', math.sqrt(x ** 2 + y ** 2))
+
+
+def lenght(x1, x2, y1, y2):
+    print('Длина отрезка:', math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2))
+
+
+choise = int(input('Что выбрать 1 или 2'))
+if choise == 1:
+    x = int(input('X: '))
+    y = int(input('Y: '))
+    distance(x, y)
+elif choise == 2:
+    x1 = int(input('X1: '))
+    y1 = int(input('Y1: '))
+    x2 = int(input('X2: '))
+    y2 = int(input('Y2: '))
+    lenght(x1, x2, y1, y2)
+else:
+    print('Wrong number')
