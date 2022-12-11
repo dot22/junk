28b80644fd4d225a85b67fb5251df79875084898
Igor Moroz <igor.moroz@gmail.com>
2022-12-11 22:24:05 +0300

    13.4.1

--- Probe.py
+++ Probe.py
@@ -1,16 +1,7 @@
-def isprime(amount):
-    summ = 0
-    for order in range(amount):
-        a = int(input("Введите число: "))
-        flag = 0
-        for i in range(2, a // 2 + 1):
-            if a % i == 0:
-                flag = 1
-                break
-        if flag == 0:
-            summ += 1
-    print('Summ =', summ)
+def middlesumm(a, b):
+    print((a + b) / 2)
 
 
-numbers = int(input('Введите количество чисел: '))
-isprime(numbers)
+a = int(input('Введите левую границу: '))
+b = int(input('Введите правую границу: '))
+middlesumm(a, b)
