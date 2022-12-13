42b169707568dcd2dafa45738c829a1376a897ab
dot22 <igor.moroz@gmail.com>
2022-12-13 16:03:39 +0300

    13.6.9

--- Probe.py
+++ Probe.py
@@ -1,14 +1,46 @@
-def nod(num1, num2):
-    while (num1 != 0) and (num2 != 0):
-        if num1 > num2:
-            num1, num2 = num2, num1 % num2
-            nd = num1
+import random
+
+
+def rock_paper_scissors():
+    machine = random.randint(1, 3)
+    user = int(input('Камень - 1, Ножницы - 2, Бумага - 3: '))
+    if (machine == 1 and user == 2) or (machine == 2 and user == 3) or (machine == 3 and user == 1):
+        print('Machine =', machine, 'User =', user)
+        print('Machine Win')
+    elif (user == 1 and machine == 2) or (user == 2 and machine == 3) or (user == 3 and machine == 1):
+        print('Machine =', machine, 'User =', user)
+        print('User Win')
+    else:
+        print('Nobody win')
+    print('\n')
+    mainMenu()
+
+
+def guess_the_number():
+    number = random.randint(1, 100)
+    print('Подсказка:', number)
+    num_min = 1
+    num_max = 100
+    while True:
+        guess = int(input('Guess the number: '))
+        if number == guess:
+            print('You are winner! Number is:', number)
+            break
         else:
-            num2, num1 = num1, num2 % num1
-            nd = num2
-    print('NOD =', nd)
+            print('Wrong number. Try again.')
+            continue
+    mainMenu()
 
 
-number1 = int(input('Number 1: '))
-number2 = int(input('Number 2: '))
-nod(number1, number2)
+def mainMenu():
+    choice = int(input('Choice the game: 1 - scissors, 2 - numbers: '))
+    if choice == 1:
+        rock_paper_scissors()
+    elif choice == 2:
+        guess_the_number()
+    else:
+        print('Wrong number. Try another.')
+        mainMenu()
+
+
+mainMenu()
