7f2cf0be1e011d9317a60bdbbf7a4a9776943733
dot22 <igor.moroz@gmail.com>
2022-12-13 14:47:12 +0300

    13.6.6

--- Probe.py
+++ Probe.py
@@ -1,16 +1,10 @@
-def count_letters(some_text):
-    count_num = 0
-    count_let = 0
-    number = input('What number: ')
-    letter = input('What letter: ')
-    for i in some_text:
-        if i == number:
-            count_num += 1
-        elif i == letter:
-            count_let += 1
-    print('Number', number, 'is:', count_num)
-    print('Letter', letter, 'is', count_let)
+def miner(x, y):
+    if -1 <= x <= 1 and -1 <= y <= y:
+        print('Nearest')
+    else:
+        print('Not here')
 
 
-text = input('Enter some text: ')
-count_letters(text)
+x = float(input('X: '))
+y = float(input('Y: '))
+miner(x, y)
