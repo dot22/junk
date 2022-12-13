969372bba9b1d0659a224e7ea9500155d9cfb9b7
dot22 <igor.moroz@gmail.com>
2022-12-13 15:08:50 +0300

    13.6.7

--- Probe.py
+++ Probe.py
@@ -1,10 +1,10 @@
-def miner(x, y):
-    if -1 <= x <= 1 and -1 <= y <= y:
-        print('Nearest')
-    else:
-        print('Not here')
+def min_num(num1, num2):
+    summ = num1 + num2
+    diff = abs(num1 - num2)
+    min = (summ - diff) / 2
+    print(min)
 
 
-x = float(input('X: '))
-y = float(input('Y: '))
-miner(x, y)
+num1 = int(input('Number 1: '))
+num2 = int(input('Nuber 2: '))
+min_num(num1, num2)
