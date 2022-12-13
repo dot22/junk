d0486b9e2c8e86d5c5c2ae110ea4a37b7921922c
dot22 <igor.moroz@gmail.com>
2022-12-13 15:26:33 +0300

    13.6.8

--- Probe.py
+++ Probe.py
@@ -1,10 +1,14 @@
-def min_num(num1, num2):
-    summ = num1 + num2
-    diff = abs(num1 - num2)
-    min = (summ - diff) / 2
-    print(min)
+def nod(num1, num2):
+    while (num1 != 0) and (num2 != 0):
+        if num1 > num2:
+            num1, num2 = num2, num1 % num2
+            nd = num1
+        else:
+            num2, num1 = num1, num2 % num1
+            nd = num2
+    print('NOD =', nd)
 
 
-num1 = int(input('Number 1: '))
-num2 = int(input('Nuber 2: '))
-min_num(num1, num2)
+number1 = int(input('Number 1: '))
+number2 = int(input('Number 2: '))
+nod(number1, number2)
