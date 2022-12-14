dc62a58280e1e2c0c13f4288cd5d8d5fc034d457
Igor Moroz <igor.moroz@gmail.com>
2022-12-14 22:05:26 +0300

    14.3.1

--- Probe.py
+++ Probe.py
@@ -1,14 +1,10 @@
-def first(tasks):
-    result = 0
-    for i in range(tasks):
-        number = int(input('Enter the number: '))
-        if number < 0:
-            number = 0
-        if number > result:
-            result = number
-    return result
+count = 0
+number = 1
 
+while number != 0:
+    previous_number = number
+    number /= 2
+    count += 1
 
-number_of_tasks = int(input('How many tasks will be: '))
-first_task = first(number_of_tasks)
-print('The first task is', first_task)
+print(previous_number)
+print(count)
