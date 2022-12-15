9ad66388915b49f3a4f7f3ac5f1eb8a231a7607d
dot22 <igor.moroz@gmail.com>
2022-12-15 16:13:56 +0300

    14.6.4

--- Probe.py
+++ Probe.py
@@ -1,17 +1,13 @@
-def num_reverse(num):
-    reverse = ''
-    for i in str(num):
-        reverse = i + reverse
+def exp_string(digit):
+    text = ''
+    for i in digit:
+        if i != 'e':
+            text = text + i
+        else:
+            mantis = text
+            text = ''
+    print(mantis, 'dev', text)
 
-    return int(reverse)
 
-
-num1 = int(input('Enter Number 1: '))
-num2 = int(input('Enter Number 2: '))
-
-num1_reverse = num_reverse(num1)
-num2_reverse = num_reverse(num2)
-
-sum_num = num1_reverse + num2_reverse
-print(num_reverse(sum_num))
-
+number = (input('Enter the number: '))
+exp_string(number)
