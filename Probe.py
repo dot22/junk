394dbd5f38f4e4219f9bd170ef492dccd26b1b43
dot22 <igor.moroz@gmail.com>
2022-12-16 15:55:38 +0300

    14.6.6

--- Probe.py
+++ Probe.py
@@ -1,36 +1,12 @@
-def count_digits(number):
-    count = 1
-    while number // 10 > 0:
+def count_range(start,stop):
+    count = 0
+    while start - stop > 1e-15:
         count += 1
-        number //= 10
-    return count
+        start -= start * 8.4 / 100
+    print(count)
 
 
-def changed_number(number, restrict):
-    if count_digits(number) < restrict:
-        print('Не хватает цифр')
-        return
-    else:
-        return between_digits(number)
+starting_point = float(input('Starting point: '))
+stop_point = float(input('Stop point: '))
 
-
-def between_digits(number):
-    count = len(str(number))
-    first = number // (10 ** (count - 1))
-    last = number % 10
-    result = (number - (first * 10 ** (count - 1)) - last + last * 10 ** (count - 1) + first)
-    print(result)
-    return result
-
-
-def sum_numbers(a, b):
-    c = int(a) + int(b)
-    return c
-
-
-c = count_digits(123)
-d = count_digits(4567)
-e = changed_number(123, 3)
-f = changed_number(4567, 4)
-g = sum_numbers(e, f)
-print(g)
+count_range(starting_point, stop_point)
