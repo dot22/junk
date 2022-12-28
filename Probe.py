0c361394b0cc7b46fec43219477fb652d87ae32f
Igor Moroz <igor.moroz@gmail.com>
2022-12-29 00:21:38 +0300

    15.5

--- Probe.py
+++ Probe.py
@@ -1,17 +1,13 @@
-print("Введите первую точку")
-x1 = float(input('X: '))
-y1 = float(input('Y: '))
-print("\nВведите вторую точку")
-x2 = float(input('X: '))
-y2 = float(input('Y: '))
-x_diff = x1 - x2
-y_diff = y1 - y2
+def sum_factorials(num):
+    part_factorial = 1
+    summa = 0
 
-print("Уравнение прямой, проходящей через эти точки:")
-if x_diff == 0:
-    b = y2
-    print("y = ", b)
-else:
-    k = y_diff / x_diff
-    b = y2 - k * x2
-    print("y = ", k, " * x + ", b)
+    for i in range(1, num + 1):
+        part_factorial *= i
+        summa += part_factorial
+
+    return summa
+
+
+n = int(input('Введите число: '))
+print(sum_factorials(n))
