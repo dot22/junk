607ccb736ac36f5cdc24ba56bb2b268cf2de7426
Igor Moroz <igor.moroz@gmail.com>
2022-12-29 00:06:43 +0300

    15.4

---
+++ Probe.py
@@ -0,0 +1,17 @@
+print("Введите первую точку")
+x1 = float(input('X: '))
+y1 = float(input('Y: '))
+print("\nВведите вторую точку")
+x2 = float(input('X: '))
+y2 = float(input('Y: '))
+x_diff = x1 - x2
+y_diff = y1 - y2
+
+print("Уравнение прямой, проходящей через эти точки:")
+if x_diff == 0:
+    b = y2
+    print("y = ", b)
+else:
+    k = y_diff / x_diff
+    b = y2 - k * x2
+    print("y = ", k, " * x + ", b)
