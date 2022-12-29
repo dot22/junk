a7c54e1900fda4db2b722f5ee2f1beab1f373d3f
dot22 <igor.moroz@gmail.com>
2022-12-29 15:16:23 +0300

    15.7.02

--- python-ds/15_IDE/02_session/main.py
+++ python-ds/15_IDE/02_session/main.py
@@ -6,14 +6,12 @@ x2 = float(input('X: '))
 y2 = float(input('Y: '))
 
 x_diff = x1 - x2
-y_diff = y1 - y2
-if x1 != x2:
+print("Уравнение прямой, проходящей через эти точки:")
+if x_diff != 0:
+    y_diff = y1 - y2
     k = y_diff / x_diff
     b = y2 - k * x2
-    print("Уравнение прямой, проходящей через эти точки:")
     print("y = ", k, " * x + ", b)
 else:
-    print("Уравнение прямой, проходящей через эти точки:")
-    print("x = ", x2)
+    print("x = ", x1)
 
-# зачёт!
