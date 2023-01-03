660dfa569a0daf6896c98ea02f3672ae91f7ec65
dot22 <igor.moroz@gmail.com>
2023-01-03 16:02:18 +0300

    16.2.2

--- Probe.py
+++ Probe.py
@@ -1,16 +1,19 @@
+nums_count = int(input('Количество чисел в списке: '))
 nums_list = []
-N = int(input('Кол-во чисел в списке: '))
-for _ in range(N):
-    num = int(input('Очередное число: '))
-    nums_list.append(num)
 
-maximum = nums_list[0]
-minimum = nums_list[0]
+for i_num in range(nums_count):
+    print('Введите', i_num + 1, 'число:', end=' ')
+    nums_list.append(int(input('')))
 
-for i in nums_list:
-    if maximum < i:
-        maximum = i
-    if minimum > i:
-        minimum = i
-print('Максимальное число в списке:', maximum)
-print('Минимальное число в списке:', minimum)
+# print(nums_list)
+
+divider = int(input('Введите делитель: '))
+
+index_sum = 0
+
+for i_num in range(nums_count):
+    if nums_list[i_num] % divider == 0:
+        print('Индекс числа ', nums_list[i_num], ': ', i_num, sep='')
+        index_sum += i_num
+
+print('Сумма индексов:', index_sum)
