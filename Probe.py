bc35e1b6263ce69464231b3a4bbbcf8ba37ee0f4
dot22 <igor.moroz@gmail.com>
2023-01-03 16:51:36 +0300

    16.3.1

--- Probe.py
+++ Probe.py
@@ -1,23 +1,21 @@
-dogs_count = int(input('Количество собак: '))
-dogs_list = []
+text = input('Введите строку: ')
+new_text = ''
 
-for i_dog in range(dogs_count):
-    print('Рейтинг собаки', i_dog + 1, '-', end=' ')
-    dogs_list.append(int(input('')))
+text_list = list(text)
+new_text_list = []
 
-print('\nНеверный рейтинг собак:')
-print(dogs_list)
+count = 0
 
-min_dog = 0
-max_dog = 0
+for i in text_list:
+    if i != ':':
+        new_text_list.append(i)
+    else:
+        count += 1
+        new_text_list.append(';')
 
-for i_dog in range(dogs_count):
-    if dogs_list[min_dog] > dogs_list[i_dog]:
-        min_dog = i_dog
-    if dogs_list[max_dog] < dogs_list[i_dog]:
-        max_dog = i_dog
+for j in new_text_list:
+    new_text += j
 
-# print(min_dog, max_dog)
-dogs_list[min_dog], dogs_list[max_dog] = dogs_list[max_dog], dogs_list[min_dog]
-print('\nВерный рейтинг собак:')
-print(dogs_list)
+print('Исправленная строка:', new_text)
+print('Количество замен:', count)
+
