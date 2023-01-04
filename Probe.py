ce9827418b07146f30cdc6f6fa4860e0dd0647ff
dot22 <igor.moroz@gmail.com>
2023-01-04 15:14:47 +0300

    17.2.1

--- Probe.py
+++ Probe.py
@@ -1,23 +1,8 @@
-word_list = []
-word_count_list = [0, 0, 0]
+zoo = ['lion', 'kangaroo', 'elephant', 'monkey']
 
-for i in range(1, 4):
-    print('Введите', i, 'слово:', end=' ')
-    word_list.append(input())
+zoo.insert(zoo.index('lion') + 1, 'bear')
 
-# print(word_list)
-
-while True:
-    text = input('Слово из текста: ')
-    if text == 'end':
-        break
-    for i in range(3):
-        if text == word_list[i]:
-            word_count_list[i] += 1
-
-# print(word_count_list)
-
-print('\nПодсчет слов в тексте')
-
-for i in range(3):
-    print(word_list[i], ': ', word_count_list[i], sep='')
+zoo.remove('elephant')
+print(zoo)
+print(zoo.index('lion'))
+print(zoo.index('monkey'))
