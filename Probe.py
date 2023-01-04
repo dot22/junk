5ccce389d047017c14a311970b4cff51d61fa176
dot22 <igor.moroz@gmail.com>
2023-01-04 14:34:46 +0300

    16.3.3

--- Probe.py
+++ Probe.py
@@ -1,16 +1,23 @@
-text = input('Введите строку: ')
-sym_number = int(input('Номер символа: '))
+word_list = []
+word_count_list = [0, 0, 0]
 
-text_list = list(text)
-count = 0
+for i in range(1, 4):
+    print('Введите', i, 'слово:', end=' ')
+    word_list.append(input())
 
-print()
-print('Символ слева:', text_list[sym_number - 2])
-print('Символ справа:', text_list[sym_number])
+# print(word_list)
 
-if text_list[sym_number - 2] == text_list[sym_number - 1] == text_list[sym_number]:
-    print('\nЕсть еще два таких символа')
-elif text_list[sym_number - 2] == text_list[sym_number - 1] or text_list[sym_number - 1] == text_list[sym_number]:
-    print('\nЕсть ровно один такой же символ')
-else:
-    print('\nТаких же символов нет')
+while True:
+    text = input('Слово из текста: ')
+    if text == 'end':
+        break
+    for i in range(3):
+        if text == word_list[i]:
+            word_count_list[i] += 1
+
+# print(word_count_list)
+
+print('\nПодсчет слов в тексте')
+
+for i in range(3):
+    print(word_list[i], ': ', word_count_list[i], sep='')
