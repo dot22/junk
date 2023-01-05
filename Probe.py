f6cae3d8cecdf01a2a5de12b0b1322ab6673665d
dot22 <igor.moroz@gmail.com>
2023-01-05 15:02:44 +0300

    17.2.2

--- Probe.py
+++ Probe.py
@@ -1,8 +1,16 @@
-zoo = ['lion', 'kangaroo', 'elephant', 'monkey']
+employes = int(input('Количество сотрудников: '))
+salary_list = []
 
-zoo.insert(zoo.index('lion') + 1, 'bear')
+for i_emp in range(employes):
+    print('Зарплата', i_emp + 1, 'сотрудника:', end=' ')
+    salary = int(input(''))
+    salary_list.append(salary)
 
-zoo.remove('elephant')
-print(zoo)
-print(zoo.index('lion'))
-print(zoo.index('monkey'))
+print(salary_list)
+
+for _ in salary_list:
+    if 0 in salary_list:
+        salary_list.remove(0)
+
+print('Осталось сотрудников:', len(salary_list))
+print('Зарплаты:', salary_list)
