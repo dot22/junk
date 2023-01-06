47cdd407f18e779e88cc6610a8d93d5ac1718a00
dot22 <igor.moroz@gmail.com>
2023-01-06 16:53:10 +0300

    17.3.3

--- Junk.py
+++ Junk.py
@@ -1,14 +1,21 @@
-# string_1 = 'Привет!'
-# string_2 = 'Как дела? Что делаешь?'
-string_1 = 'Хм!!'
-string_2 = '?'
+pacs_amount = int(input('Количество пакетов: '))
+message = []
+wrong_packs_amount = 0
 
-print('Первое сообщение:', string_1)
-print('Второе сообщение:', string_2)
+for i in range(pacs_amount):
+    print('Пакет номер', i + 1)
+    packet = []
+    for j in range(4):
+        print(j + 1, 'пакет', end=' ')
+        bit = int(input())
+        packet.append(bit)
+    if packet.count(-1) <= 1:
+        message.extend(packet)
+    else:
+        wrong_packs_amount += 1
+        print('Много ошибок в пакете')
+    print()
 
-if string_1.count('!') + string_1.count('?') > string_2.count('!') + string_2.count('?'):
-    print(string_1 + string_2)
-elif string_1.count('!') + string_1.count('?') < string_2.count('!') + string_2.count('?'):
-    print(string_2 + string_1, sep=' ')
-else:
-    print('Ой!')
+print('Полученное сообщение:', message)
+print('Количество ошибок в сообщении:', message.count(-1))
+print('Количество потерянных пакетов:', wrong_packs_amount)
