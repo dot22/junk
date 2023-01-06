ce7b55d64d69a87e2b7e1b838ea9a6dc70ba28c3
dot22 <igor.moroz@gmail.com>
2023-01-06 17:19:16 +0300

    17.4.2

--- Probe.py
+++ Probe.py
@@ -1,6 +1,15 @@
-matrix = [[1, 2 ,3], [4, 5, 6], [7, 8, 9]]
+# members = int(input('Общее количество участников: '))
+# command = int(input('Количество человек в команде: '))
 
-for row in matrix:
-    for col in row:
-        print(col, end=' ')
-    print()
+members = 12
+command = 5
+command_list = []
+start = 1
+
+if members % command == 0:
+    for i in range(members // command):
+        command_list.append(list(range(start, start + command)))
+        start += command
+    print('Общий список участников:', command_list)
+else:
+    print(members, 'участников невозможно поделить на команды по', command, 'человек')
