e99f62251e9c4c08511157c02aeed7aff62ba6f5
dot22 <igor.moroz@gmail.com>
2023-01-09 16:36:55 +0300

    17.6.5

--- Probe.py
+++ Probe.py
@@ -1,19 +1,25 @@
-guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
+violator_songs = [
+    ['World in My Eyes', 4.86],
+    ['Sweetest Perfection', 4.43],
+    ['Personal Jesus', 4.56],
+    ['Halo', 4.9],
+    ['Waiting for the Night', 6.07],
+    ['Enjoy the Silence', 4.20],
+    ['Policy of Truth', 4.76],
+    ['Blue Dress', 4.29],
+    ['Clean', 5.83]
+]
 
-while True:
-    print('Сейчас на вечеринке', len(guests), 'человек:', guests)
-    question = input('Гость пришел или ушел? ')
-    if question == 'пора спать':
-        print('\nВечеринка закончилась, все легли спать.')
-        break
-    elif question == 'пришел':
-        name = input('Имя гостя:')
-        if len(guests) >= 6:
-            print('Прости, ', name, ', но мест нет.', sep='')
-        else:
-            print('Привет,', name)
-            guests.append(name)
-    else:
-        name = input('Имя гостя: ')
-        print('Пока,', name)
-        guests.remove(name)
+songs = 3
+time = 0
+
+for i_song in range(1, songs + 1):
+    print('Название ', i_song, '-й песни: ', sep='', end='')
+    song_title = input()
+    for song in violator_songs:
+        # print(song)
+        if song[0] == song_title:
+            time += song[1]
+
+print('Общее время звучания песен:', round(time, 2))
+
