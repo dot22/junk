a811ea7396828f45e2873ad13311e5b44d08dad4
dot22 <igor.moroz@gmail.com>
2023-01-09 15:42:54 +0300

    17.6.2

--- Probe.py
+++ Probe.py
@@ -1,14 +1,7 @@
-main_list = [1, 5, 3]
-ext_list_b = [1, 5, 1, 5]
-ext_list_c = [1, 3, 1, 5, 3, 3]
-
-main_list.extend(ext_list_b)
-print('Количество цифр 5 при объединении:', main_list.count(5))
-
-while 5 in main_list:
-    main_list.remove(5)
-
-main_list.extend(ext_list_c)
-print('Количество цифр 3 при втором объединении:', main_list.count(3))
-
-print('Итоговый список:', main_list)
+list_1 = list(range(160, 178, 2))
+print('Первый класс:', list_1)
+list_2 = list(range(162,181, 3))
+print('Второй класс:', list_2)
+list_1.extend(list_2)
+list_1.sort()
+print('Объединенный класс:', list_1)
