99e064512d3689e3768080738e72db61d627bbbe
dot22 <igor.moroz@gmail.com>
2023-01-09 17:02:34 +0300

    17.6.6 - imtermediate

--- Probe.py
+++ Probe.py
@@ -1,25 +1,7 @@
-violator_songs = [
-    ['World in My Eyes', 4.86],
-    ['Sweetest Perfection', 4.43],
-    ['Personal Jesus', 4.56],
-    ['Halo', 4.9],
-    ['Waiting for the Night', 6.07],
-    ['Enjoy the Silence', 4.20],
-    ['Policy of Truth', 4.76],
-    ['Blue Dress', 4.29],
-    ['Clean', 5.83]
-]
+first_list = [1, 2, 3]
+print('Первый список:', first_list)
+second_list = [2, 4, 6, 3, 3, 2, 1]
+print('Второй список', second_list)
 
-songs = 3
-time = 0
-
-for i_song in range(1, songs + 1):
-    print('Название ', i_song, '-й песни: ', sep='', end='')
-    song_title = input()
-    for song in violator_songs:
-        # print(song)
-        if song[0] == song_title:
-            time += song[1]
-
-print('Общее время звучания песен:', round(time, 2))
-
+first_list.extend(second_list)
+print('Расширенный первый список:', first_list)
