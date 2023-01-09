9a96a3a41f94e39492fbfee03f26dee4a4a207a8
dot22 <igor.moroz@gmail.com>
2023-01-09 15:47:33 +0300

    17.6.3

--- Probe.py
+++ Probe.py
@@ -1,7 +1,15 @@
-list_1 = list(range(160, 178, 2))
-print('Первый класс:', list_1)
-list_2 = list(range(162,181, 3))
-print('Второй класс:', list_2)
-list_1.extend(list_2)
-list_1.sort()
-print('Объединенный класс:', list_1)
+shop = [['каретка', 1200], ['шатун', 1000], ['седло', 300], ['педаль', 100],
+        ['седло', 1500], ['рама', 12000], ['обод', 2000], ['шатун', 200], ['седло', 2700]]
+
+detail = 'седло'
+amount = 0
+cost = 0
+
+for i_detail in shop:
+        if i_detail[0] == detail:
+                amount += 1
+                cost += i_detail[1]
+
+print('Название детали:', detail)
+print('Количество деталей:', amount)
+print('Общая стоимость:', cost)
