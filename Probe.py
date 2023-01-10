7dd47f1d3b1aeebbe9aa86890bcafc33138de909
dot22 <igor.moroz@gmail.com>
2023-01-10 16:20:44 +0300

    17.6.6

--- Probe.py
+++ Probe.py
@@ -5,3 +5,9 @@ print('Второй список', second_list)
 
 first_list.extend(second_list)
 print('Расширенный первый список:', first_list)
+
+for i in first_list:
+    for j in range(first_list.count(i) - 1):
+        first_list.remove(i)
+
+print(first_list)
